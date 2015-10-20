f933f40e06669a1dba4a333911a603114b40c883
drypot <drypot@gmail.com>
2015-10-20 16:20:43 +0900

    rename mig-v1 dir to mig-1.

--- server/mig-1/mig-1-user.js
+++ server/mig-1/mig-1-user.js
@@ -1,3 +1,30 @@
+var init = require('../base/init');
+var config = require('../base/config');
+var sql = require("seriate");
+
+init.run(function (err) {
+  if (err) throw err;
+});
+
+// Change the config settings to match your 
+// SQL Server and database
+var config = {  
+    "server": "127.0.0.1",
+    "user": "nodejs",
+    "password": "mypassword",
+    "database": "mydatabase"
+};
+
+sql.setDefaultConfig( config );
+
+sql.execute( {  
+        query: "SELECT * FROM INFORMATION_SCHEMA.TABLES"
+    } ).then( function( results ) {
+        console.log( results );
+    }, function( err ) {
+        console.log( "Something bad happened:", err );
+    } );
+
 /* TODO
 
 table Users (
