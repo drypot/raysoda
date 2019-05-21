1f7b7da5a808e7111defeafb082775b685b2af3c
drypot <drypot@gmail.com>
2019-05-21 16:04:09 +0900

    move image-script to mongodb-script.

---
+++ app/mongodb-script/mig-drypot-files.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const exec = require('child_process').exec;
+const fs = require('fs');
+const tds = require('tedious');
+const types = tds.TYPES;
+const init = require('../base/init');
+const config = require('../base/config');
+const fs2 = require('../base/fs2');
+const mongo2 = require('../mongodb/mongo2');
+const imageb = require('../image/image-base');
+
+/* 
+  먼저 public 디렉토리를 public-old 로 변경해 놓는다.
+  구 디렉토리에서 새 디렉토리로 필요한 파일만 이동한다.
+
+  $ node app/image-script/mig-osoky-files.js -c config/drypot-dev.json 1 10
+*/
+
+init.main(function (done) {
+  if (config.argv._.length != 2) {
+    console.log('Start and end ids should be specified.');
+    return done();
+  }
+  var s = parseInt(config.argv._[0]);
+  var e = parseInt(config.argv._[1]);
+  var i = s;
+  (function loop() {
+    if (i > e) {
+      mongo2.db.close();
+      return done();
+    }
+    doIt(i++, function (err) {
+      if (err) return done(err);
+      setImmediate(loop);
+    });
+  })();
+});
+
+function doIt(id, done) {
+  var src = config.uploadDir + '/public-old/images/' + fs2.makeDeepPath(id, 3) + '/' + id + '-org.svg';
+  fs.access(src, function (err) {
+    if (err) return done();
+    console.log(src);
+    imageb.checkImageMeta(src, function (err, meta) {
+      if (err) return done(err);
+      imageb.saveImage(id, src, meta, done);
+    });
+  });
+}
