be1266034385a73822ba1eebcd5ddfdd86f844fe
drypot <drypot@gmail.com>
2021-02-26 02:42:43 +0900

    add aws ses support.

--- code/mailer/mailer2.js
+++ code/mailer/mailer2.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require('fs');
 const nodemailer = require('nodemailer');
 
 const init = require('../base/init');
@@ -9,18 +10,22 @@ const mailer2 = exports;
 var transport;
 
 init.add(function (done) {
-  
-  // 일단은 postfix 통해서 메일 보내는 것으로 유지
-  // To Do: 메일 발송 테스트를 위해 구글 계정을 사용하는 코드를 추가
 
-  if (config.mailServer) {
-    transport = nodemailer.createTransport({
-      host: config.mailServer,
-      port: 25
+  if (config.mailServer === "aws") {
+    fs.readFile("config-live/ses-smtp-user.json", 'utf8', function (err, data) {
+      if (err) return done(err);
+      transport = nodemailer.createTransport(JSON.parse(data));
+      done();
     });
+  } else {
+    if (config.mailServer) {
+      transport = nodemailer.createTransport({
+        host: config.mailServer,
+        port: 587
+      });
+    }
+    done();
   }
-
-  done();
 });
 
 mailer2.send = function (opt, done) {
