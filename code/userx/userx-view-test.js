2ceced18cc9f056e4ac547974c8d4b398b5eb387
drypot <drypot@gmail.com>
2021-03-17 23:34:35 +0900

    change module style. userx done.

--- code/userx/userx-view-test.js
+++ code/userx/userx-view-test.js
@@ -1,18 +1,16 @@
-'use strict';
-
-const assert = require('assert');
-const init = require('../base/init');
-const config = require('../base/config');
-const my2 = require('../mysql/my2');
-const expb = require('../express/express-base');
-const expl = require('../express/express-local');
-const userb = require('../user/user-base');
-const userf = require('../user/user-fixture');
-const userxv = require('../userx/userx-view');
+import * as assert2 from "../base/assert2.js";
+import * as init from "../base/init.js";
+import * as config from "../base/config.js";
+import * as db from '../db/db.js';
+import * as expb from '../express/express-base.js';
+import * as expl from '../express/express-local.js';
+import * as userb from '../user/user-base.js';
+import * as userf from '../user/user-fixture.js';
+import * as userxv from "../userx/userx-view.js";
 
 before(function (done) {
-  config.path = 'config/raysoda-test.json';
-  my2.dropDatabase = true;
+  config.setPath('config/raysoda-test.json');
+  db.setDropDatabase(true);
   init.run(done);
 });
 
@@ -24,8 +22,8 @@ before((done) => {
 describe('get /users/:id([0-9]+)', function () {
   it('should succeed', function (done) {
     expl.get('/users/1').end(function (err, res) {
-      assert.ifError(err);
-      assert.ifError(res.body.err);
+      assert2.ifError(err);
+      assert2.ifError(res.body.err);
       done();
     });
   });
@@ -34,21 +32,21 @@ describe('get /users/:id([0-9]+)', function () {
 describe('get /:name([^/]+)', function () {
   it('should succeed for /user1', function (done) {
     expl.get('/user1').end(function (err, res) {
-      assert.ifError(err);
-      assert.ifError(res.body.err);
+      assert2.ifError(err);
+      assert2.ifError(res.body.err);
       done();
     });
   });
   it('should succeed for /USER1', function (done) {
     expl.get('/USER1').end(function (err, res) {
-      assert.ifError(err);
-      assert.ifError(res.body.err);
+      assert2.ifError(err);
+      assert2.ifError(res.body.err);
       done();
     });
   });
   it('should fail with invalid name', function (done) {
     expl.get('/xman').end(function (err, res) {
-      assert(err !== null);
+      assert2.ok(err !== null);
       done();
     });
   });
--- code/userx/userx-view.js
+++ code/userx/userx-view.js
@@ -1,36 +1,35 @@
-'use strict';
-
-const async = require('../base/async');
-const url2 = require('../base/url2');
-const date2 = require('../base/date2');
-const my2 = require('../mysql/my2');
-const expb = require('../express/express-base');
-const userb = require('../user/user-base');
-const imageb = require('../image/image-base');
-const imagel = require('../image/image-list');
+import * as assert2 from "../base/assert2.js";
+import * as async2 from "../base/async2.js";
+import * as date2 from "../base/date2.js";
+import * as url2 from "../base/url2.js";
+import * as db from '../db/db.js';
+import * as expb from '../express/express-base.js';
+import * as userb from '../user/user-base.js';
+import * as imageb from '../image/image-base.js';
+import * as imagel from '../image/image-list.js';
 
 expb.core.get('/users/:id([0-9]+)', function (req, res, done) {
-  var id = parseInt(req.params.id) || 0;
+  const id = parseInt(req.params.id) || 0;
   userb.getCached(id, function (err, tuser) {
     if (err) return done(err);
-    list(req, res, tuser);
+    list(req, res, tuser, done);
   });
 });
 
 expb.core.get('/:name([^/]+)', function (req, res, done) {
-  var home = decodeURIComponent(req.params.name);
+  const home = decodeURIComponent(req.params.name);
   userb.getCachedByHome(home, function (err, tuser) {
     if (err) return done(err);
     if (!tuser) return done();
-    list(req, res, tuser);
+    list(req, res, tuser, done);
   });
 });
 
-function list(req, res, tuser) {
-  var user = res.locals.user;
-  var p = Math.max(parseInt(req.query.p) || 1, 1);
-  var ps = Math.min(Math.max(parseInt(req.query.ps) || 16, 1), 128);
-  my2.query('select * from image where uid = ? order by cdate desc limit ?, ?', [tuser.id, (p-1)*ps, ps], (err, images) => {
+function list(req, res, tuser, done) {
+  const user = res.locals.user;
+  const p = Math.max(parseInt(req.query.p) || 1, 1);
+  const ps = Math.min(Math.max(parseInt(req.query.ps) || 16, 1), 128);
+  db.query('select * from image where uid = ? order by cdate desc limit ?, ?', [tuser.id, (p-1)*ps, ps], (err, images) => {
     if (err) return done(err);
     imagel.decoImageList(images, (err) => {
       if (err) return done(err);
