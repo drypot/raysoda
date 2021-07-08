e7db41bd0af4055459cad4e7f191f265f07e56ea
drypot <drypot@gmail.com>
2021-07-09 00:11:12 +0900

    migrating to jasmine, assert2.

--- src/base/assert2-test.mjs
+++ src/base/assert2-test.mjs
@@ -1,45 +1,44 @@
-import * as assert2 from "../base/assert2.mjs";
+import * as assert2 from '../base/assert2.mjs'
 
 describe('aliases', () => {
   it('should succeed', done => {
-    expect('abc').toBe('abc');
-    assert2.e('abc', 'abc');
-    assert2.ne('abc', 'def');
-    assert2.de([1], [1]);
-    assert2.nde([1], [2]);
-    done();
-  });
-});
+    assert2.e('abc', 'abc')
+    assert2.ne('abc', 'def')
+    assert2.de([1], [1])
+    assert2.nde([1], [2])
+    done()
+  })
+})
 
 describe('empty', () => {
   it('should succeed', done => {
-    assert2.empty(undefined);
-    assert2.empty(null);
-    assert2.empty({});
+    assert2.empty(undefined)
+    assert2.empty(null)
+    assert2.empty({})
     assert2.throws(function () {
-      assert2.empty({ a: 1 });
-    });
-    done();
-  });
-});
+      assert2.empty({ a: 1 })
+    })
+    done()
+  })
+})
 
 describe('notEmpty', () => {
   it('should succeed', done => {
-    assert2.notEmpty({ a: 1 });
+    assert2.notEmpty({ a: 1 })
     assert2.throws(function () {
-      assert2.notEmpty({});
-    });
-    done();
-  });
-});
+      assert2.notEmpty({})
+    })
+    done()
+  })
+})
 
-describe('path', () => {
+describe('pathExists', () => {
   it('should succeed', done => {
-    assert2.pathExists('src/base/assert2.mjs');
-    assert2.pathNotExists('src/base/assertX.mjs');
-    done();
-  });
-});
+    assert2.pathExists('src/base/assert2.mjs')
+    assert2.pathNotExists('src/base/assertX.mjs')
+    done()
+  })
+})
 
 describe('redirect', () => {
   it('should succeed', done => {
@@ -48,13 +47,13 @@ describe('redirect', () => {
       header: {
         location: '/new301'
       }
-    }, '/new301');
+    }, '/new301')
     assert2.redirect({
       status: 302,
       header: {
         location: '/new302'
       }
-    }, '/new302');
-    done();
-  });
-});
+    }, '/new302')
+    done()
+  })
+})
--- src/base/assert2.mjs
+++ src/base/assert2.mjs
@@ -1,48 +1,48 @@
-import * as fs from "fs";
-import * as assert from "assert";
+import * as fs from 'fs'
+import * as assert from 'assert'
 
-export * from "assert";
+export * from 'assert'
 
-export const e = assert.strictEqual;
-export const ne = assert.notStrictEqual;
-export const de = assert.deepStrictEqual;
-export const nde = assert.notDeepStrictEqual;
+export const e = assert.strictEqual
+export const ne = assert.notStrictEqual
+export const de = assert.deepStrictEqual
+export const nde = assert.notDeepStrictEqual
 
 function isEmpty(obj) {
   return typeof obj === 'undefined' || obj === null ||
-    (Object.keys(obj).length === 0 && obj.constructor === Object);
+    (Object.keys(obj).length === 0 && obj.constructor === Object)
 }
 
 export function empty(obj) {
   if (!isEmpty(obj)) {
-    assert.fail(obj + ' should be empty');
+    assert.fail(obj + ' should be empty')
   }
 }
 
 export function notEmpty(obj) {
   if (isEmpty(obj)) {
-    assert.fail(obj + ' should not be empty');
+    assert.fail(obj + ' should not be empty')
   }
 }
 
-export function pathExists(path)  {
+export function pathExists(path) {
   if (!fs.existsSync(path)) {
-    assert.fail(path + ' should exist.');
+    assert.fail(path + ' should exist.')
   }
 }
 
-export function pathNotExists(path)  {
+export function pathNotExists(path) {
   if (fs.existsSync(path)) {
-    assert.fail(path + ' should not exist.');
+    assert.fail(path + ' should not exist.')
   }
 }
 
 export function redirect(res, url) {
-  let codes = [301, 302];
+  let codes = [301, 302]
   if (codes.indexOf(res.status) === -1) {
-    assert.fail('invalid status code.');
+    assert.fail('invalid status code.')
   }
   if (res.header['location'] !== url) {
-    assert.fail('redirect url mismatch.');
+    assert.fail('redirect url mismatch.')
   }
 }
