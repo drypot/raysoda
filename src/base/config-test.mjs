9bd5a5d27a445027e2d5a85ae0581e9264a16f9f
drypot <drypot@gmail.com>
2021-07-09 20:37:07 +0900

    migrating to jasmine. config.mjs.

--- src/base/config-test.mjs
+++ src/base/config-test.mjs
@@ -1,19 +1,11 @@
-import * as assert2 from "../base/assert2.mjs";
-import * as init from "../base/init.mjs";
-import * as config from "../base/config.mjs";
+import { loadConfig } from './config.mjs'
 
-beforeAll(() => {
-  config.setPath('config/test.json');
-});
-
-describe('config with valid path', () => {
-  it('should succeed', done => {
-    init.run(function (err) {
-      assert2.ifError(err);
-      assert2.ne(config.prop.appName, undefined);
-      assert2.e(config.prop.xxx, undefined);
-      done();
-    });
-  });
-});
-
+describe('loadConfig', () => {
+  it('should succeed', () => {
+    const config = loadConfig('src/base/config-test-fixture.json')
+    expect(config.appName).toBe('ConfigTest')
+    expect(config.appNamel).toBe('configtest')
+    expect(config.dev).toBeTrue()
+    expect(config.xxx).toBeUndefined()
+  })
+})
