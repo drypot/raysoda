e6227fd08072d1fcd4b497391e3c0482376fc700
drypot <drypot@gmail.com>
2021-07-09 23:42:40 +0900

    migrating to jasmine. date.mjs.

---
+++ src/base/date.mjs
@@ -0,0 +1,32 @@
+export function getDate() {
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  return today
+}
+
+export function getDateFrom(s) {
+  const d = new Date(s)
+  d.setHours(0, 0, 0, 0)
+  return d
+}
+
+function pad(number) {
+  let r = String(number)
+  if (r.length === 1) {
+    r = '0' + r
+  }
+  return r
+}
+
+export function dateTimeString(d) {
+  return d.getFullYear() + '-' + pad(d.getMonth() + 1) + '-' + pad(d.getDate()) + ' ' +
+    pad(d.getHours()) + ':' + pad(d.getMinutes()) + ':' + pad(d.getSeconds())
+}
+
+export function dateString(d) {
+  return d.getFullYear() + '-' + pad(d.getMonth() + 1) + '-' + pad(d.getDate())
+}
+
+export function dateStringNoDash(d) {
+  return d.getFullYear() + pad(d.getMonth() + 1) + pad(d.getDate())
+}
