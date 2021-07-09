8ee278e8150159ca67186855deb61b19f2d6aaf2
drypot <drypot@gmail.com>
2021-07-10 03:02:06 +0900

    migrating to jasmine. fs.mjs.

---
+++ src/base/fs-test.mjs
@@ -0,0 +1,165 @@
+import * as fs from 'fs'
+import * as fs2 from './fs.mjs'
+import { pathExists, pathNotExists } from './assert.mjs'
+
+const testDir = 'tmp/fs-test'
+
+function testPath(path) {
+  return testDir + '/' + path
+}
+
+function genFiles() {
+  fs.mkdirSync(testPath('sub1'), { recursive: true, mode: 0o755 })
+  fs.mkdirSync(testPath('sub2/sub3'), { recursive: true, mode: 0o755 })
+  fs.writeFileSync(testPath('sub1/f1.txt'), 'abc')
+  fs.writeFileSync(testPath('sub2/f2.txt'), 'abc')
+  fs.writeFileSync(testPath('sub2/sub3/f3.txt'), 'abc')
+  pathExists(testPath('sub1'))
+  pathExists(testPath('sub2'))
+  pathExists(testPath('sub2/sub3'))
+  pathExists(testPath('sub1/f1.txt'))
+  pathExists(testPath('sub2/f2.txt'))
+  pathExists(testPath('sub2/sub3/f3.txt'))
+}
+
+describe('removeDir', () => {
+  beforeEach(genFiles)
+  it('should work for one file', done => {
+    fs2.removeDir(testPath('sub1/f1.txt'), err => {
+      expect(err).toBeFalsy()
+      pathExists(testPath('sub1'))
+      pathExists(testPath('sub2'))
+      pathExists(testPath('sub2/sub3'))
+      pathExists(testPath('sub2/f2.txt'))
+      pathExists(testPath('sub2/sub3/f3.txt'))
+      pathNotExists(testPath('sub1/f1.txt'))
+      done()
+    })
+  })
+  it('should work for one dir', done => {
+    fs2.removeDir(testPath('sub1'), err => {
+      expect(err).toBeFalsy()
+      pathExists(testPath('sub2'))
+      pathExists(testPath('sub2/sub3'))
+      pathExists(testPath('sub2/f2.txt'))
+      pathExists(testPath('sub2/sub3/f3.txt'))
+      pathNotExists(testPath('sub1'))
+      pathNotExists(testPath('sub1/f1.txt'))
+      done()
+    })
+  })
+  it('should work recursively', done => {
+    fs2.removeDir(testPath('sub2'), err => {
+      expect(err).toBeFalsy()
+      pathExists(testPath('sub1'))
+      pathExists(testPath('sub1/f1.txt'))
+      pathNotExists(testPath('sub2'))
+      pathNotExists(testPath('sub2/sub3'))
+      pathNotExists(testPath('sub2/f2.txt'))
+      pathNotExists(testPath('sub2/sub3/f3.txt'))
+      done()
+    })
+  })
+  it('can return ENOENT', done => {
+    fs2.removeDir(testPath('sub1/fx.txt'), err => {
+      expect(err).toBeDefined()
+      expect(err.code).toBe('ENOENT')
+      done()
+    })
+  })
+})
+
+describe('emptyDir', () => {
+  beforeAll(genFiles)
+  it('should succeed', done => {
+    const files = fs.readdirSync(testDir)
+    expect(files.length).toBeGreaterThan(0)
+    fs2.emptyDir(testDir, err => {
+      expect(err).toBeFalsy()
+      const files = fs.readdirSync(testDir)
+      expect(files.length).toBe(0)
+      done()
+    })
+  })
+})
+
+describe('makeDirs', () => {
+  beforeEach(done => {
+    fs2.emptyDir(testDir, done)
+  })
+  it('should succeed', done => {
+    pathNotExists(testPath('sub1'))
+    fs2.makeDirs(testPath('sub1'), function (err) {
+      expect(err).toBeFalsy()
+      pathExists(testPath('sub1'))
+      pathNotExists(testPath('sub1/sub2/sub3'))
+      fs2.makeDirs(testPath('sub1/sub2/sub3'), function (err) {
+        expect(err).toBeFalsy()
+        pathExists(testPath('sub1/sub2/sub3'))
+        done()
+      })
+    })
+  })
+})
+
+describe('safeFilename', () => {
+  it('should succeed', () => {
+    const table = [
+      ['`', '`'], ['~', '~'],
+      ['!', '!'], ['@', '@'], ['#', '#'], ['$', '$'], ['%', '%'],
+      ['^', '^'], ['&', '&'], ['*', '_'], ['(', '('], [')', ')'],
+      ['-', '-'], ['_', '_'], ['=', '='], ['+', '+'],
+      ['[', '['], ['[', '['], [']', ']'], [']', ']'], ['\\', '_'], ['|', '_'],
+      [';', ';'], [':', '_'], ['\'', '\''], ['"', '_'],
+      [',', ','], ['<', '_'], ['.', '.'], ['>', '_'], ['/', '_'], ['?', '_'],
+      ['aaa\tbbb', 'aaa_bbb'],
+      ['abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890', 'abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890'],
+      ['이상한 \'한글\' 이름을 가진 파일', '이상한 \'한글\' 이름을 가진 파일']
+    ]
+    for (const pair of table) {
+      const a = fs2.genSafeFilename(pair[0])
+      const b = pair[1]
+      //console.log((a === b ? 'good:' : 'bad:') + pair[0])
+      expect(a).toBe(b)
+    }
+  })
+})
+
+describe('genDeepPath', () => {
+  it('should succeed', () => {
+    expect(fs2.genDeepPath(1, 3)).toBe('0/0/1')
+    expect(fs2.genDeepPath(999, 3)).toBe('0/0/999')
+    expect(fs2.genDeepPath(1000, 3)).toBe('0/1/0')
+    expect(fs2.genDeepPath(1999, 3)).toBe('0/1/999')
+    expect(fs2.genDeepPath(999999, 3)).toBe('0/999/999')
+    expect(fs2.genDeepPath(1999999, 3)).toBe('1/999/999')
+    expect(fs2.genDeepPath(999999999, 3)).toBe('999/999/999')
+    expect(fs2.genDeepPath(9999999999, 3)).toBe('9999/999/999')
+  })
+})
+
+describe('copyFile', () => {
+  beforeAll(done => {
+    fs2.emptyDir(testDir, done)
+  })
+  it('should succeed', done => {
+    const t = testPath('fs-test-dummy-copy.txt')
+    pathNotExists(t)
+    fs2.copyFile('src/base/fs-test-dummy.txt', t, err => {
+      expect(err).toBeFalsy()
+      pathExists(t)
+      expect(fs.readFileSync(t, 'utf8')).toBe('fs2 test dummy')
+      done()
+    })
+  })
+  it('should fail when source not exist', done => {
+    const t = testPath('fs2-test-dummy-xxx-copy.txt')
+    pathNotExists(t)
+    fs2.copyFile('src/base/fs2-test-dummy-xxx.txt', t, err => {
+      expect(err).toBeDefined()
+      expect(err.code).toBe('ENOENT')
+      pathNotExists(t)
+      done()
+    })
+  })
+})
---
+++ src/base/fs.mjs
@@ -0,0 +1,101 @@
+import * as fs from 'fs'
+import * as path from 'path'
+
+export function removeDir(p, done) {
+  fs.stat(p, function (err, stat) {
+    if (err) return done(err)
+    if (stat.isFile()) {
+      return fs.unlink(p, function (err) {
+        if (err && err.code !== 'ENOENT') return done(err)
+        done()
+      })
+    }
+    if (stat.isDirectory()) {
+      fs.readdir(p, function (err, fnames) {
+        if (err) return done(err)
+        let i = 0
+
+        function unlink() {
+          if (i === fnames.length) {
+            return fs.rmdir(p, function (err) {
+              if (err && err.code !== 'ENOENT') return done(err)
+              done()
+            })
+          }
+          let fname = fnames[i++]
+          removeDir(p + '/' + fname, function (err) {
+            if (err) return done(err)
+            setImmediate(unlink)
+          })
+        }
+
+        unlink()
+      })
+    }
+  })
+}
+
+export function emptyDir(p, done) {
+  fs.readdir(p, function (err, fnames) {
+    if (err) return done(err)
+    let i = 0
+
+    function unlink() {
+      if (i === fnames.length) {
+        return done()
+      }
+      let fname = fnames[i++]
+      removeDir(p + '/' + fname, function (err) {
+        setImmediate(unlink)
+      })
+    }
+
+    unlink()
+  })
+}
+
+export function makeDirs(p, done) {
+  fs.mkdir(p, { recursive: true, mode: 0o755 }, done)
+}
+
+export function genSafeFilename(name) {
+  let i = 0
+  const len = name.length
+  let safe = ''
+  for (; i < len; i++) {
+    const ch = name.charAt(i)
+    const code = name.charCodeAt(i)
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9') || '`~!@#$%^&()-_+=[{]};\',. '.indexOf(ch) >= 0)
+      safe += ch
+    else if (code < 128)
+      safe += '_'
+    else
+      safe += ch
+  }
+  return safe
+}
+
+export function genDeepPath(id, iter) {
+  let path = ''
+  for (iter--; iter > 0; iter--) {
+    path = '/' + id % 1000 + path
+    id = Math.floor(id / 1000)
+  }
+  return id + path
+}
+
+export function copyFile(src, tar, done) {
+  const r = fs.createReadStream(src)
+  const w = fs.createWriteStream(tar)
+  r.on('error', function (err) {
+    fs.unlinkSync(tar)
+    done(err)
+  })
+  w.on('finish', function () {
+    done()
+  })
+  w.on('error', function (err) {
+    done(err)
+  })
+  r.pipe(w)
+}
