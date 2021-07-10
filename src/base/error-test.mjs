6099f36631d717443fa96edbda9066fd187baa13
drypot <drypot@gmail.com>
2021-07-10 22:10:58 +0900

    migrating to jasmine. error.mjs.

--- src/base/error-test.mjs
+++ src/base/error-test.mjs
@@ -1,85 +1,40 @@
-import * as assert2 from "../base/assert2.mjs";
-import * as init from "../base/init.mjs";
-import * as error from "../base/error.mjs";
+import { errorExists, FormError, INVALID_DATA, UNKNOWN_ERROR } from './error.mjs'
 
-beforeAll(done => {
-  init.run(done);
-});
-
-beforeAll(() => {
-  error.define('NAME_DUPE', '이미 등록되어 있는 이름입니다.', 'name');
-  error.define('PASSWORD_EMPTY', '비밀번호를 입력해 주십시오.', 'password');
-});
-
-describe('defining duplicated', () => {
-  it('should fail', done => {
-    assert2.throws(function() {
-      error.define('NAME_DUPE', '이미 등록되어 있는 이름입니다.', 'name');
-    });
-    done();
-  });
-});
-
-describe('error.newError(string)', () => {
-  it('should succeed', () => {
-    const err = error.newError('INVALID_DATA');
-    assert2.e(err.code, error.get('INVALID_DATA').code);
-    assert2.e(err.message, error.get('INVALID_DATA').message);
-    assert2.ne(err.stack, undefined);
-  });
-});
-
-describe('error.newError(field error)', () => {
-  it('should succeed', () => {
-    const err = error.newError('NAME_DUPE');
-    assert2.e(err.code, error.get('INVALID_FORM').code);
-    assert2.de(err.errors[0], error.get('NAME_DUPE'));
-    assert2.ne(err.stack, undefined);
+describe('FormError', () => {
+  it('can be created', () => {
+    const err = new FormError('ERR', 'Message', 'Field')
+    expect(err.code).toBe('ERR')
+    expect(err.message).toBe('Message')
+    expect(err.field).toBe('Field')
   })
-});
-
-describe('error.newError(unknown)', () => {
-  it('should succeed', () => {
-    const obj = {opt: 'extra'};
-    const err = error.newError(obj);
-    assert2.e(err.code, undefined);
-    assert2.e(err.message, 'unknown error');
-    assert2.e(err.opt, 'extra')
-    assert2.ne(err.stack, undefined);
-  });
-});
-
-describe('error.newError(field error)', () => {
-  it('should succeed', () => {
-    const err = error.newError('NAME_DUPE');
-    assert2.e(err.code, error.get('INVALID_FORM').code);
-    assert2.de(err.errors[0], error.get('NAME_DUPE'));
+  it('can spawn new object', () => {
+    const err = new FormError('ERR', 'Message', 'Field')
+    const err2 = err.spawn()
+    expect(err2).toEqual(err)
+    err2.opt = 'OPT'
+    expect(err.opt).toBeUndefined()
+    expect(err2.opt).toBe('OPT')
   })
-});
+})
 
-describe('error.newError(field error array)', () => {
-  it('should succeed', () => {
-    const errors = [];
-    errors.push(error.get('NAME_DUPE'));
-    errors.push(error.get('PASSWORD_EMPTY'));
-    const err = error.newError(errors);
-    assert2.e(err.code, error.get('INVALID_FORM').code);
-    assert2.de(err.errors[0], error.get('NAME_DUPE'));
-    assert2.de(err.errors[1], error.get('PASSWORD_EMPTY'));
+describe('Default Errors', () => {
+  it('exists', () => {
+    expect(UNKNOWN_ERROR).toBeDefined()
+    expect(INVALID_DATA).toBeDefined()
   })
-});
+})
 
-describe('error.find', () => {
-  it('should succeed', () => {
-    const err = error.newError('INVALID_DATA');
-    assert2.ok(error.find(err, 'INVALID_DATA'));
-    assert2.ok(!error.find(err, 'INVALID_FORM'));
-    assert2.ok(!error.find(err, 'NAME_DUPE'));
-  });
-  it('form error should succeed', () => {
-    const err = error.newError('NAME_DUPE');
-    assert2.ok(!error.find(err, 'INVALID_DATA'));
-    assert2.ok(!error.find(err, 'INVALID_FORM'));
-    assert2.ok(error.find(err, 'NAME_DUPE'));
-  });
-});
+describe('errorExists', () => {
+  const err1 = new FormError('E1')
+  const err2 = new FormError('E2')
+  const err3 = new FormError('E3')
+  const errs = [ err1, err2 ]
+  it('should work for single', () => {
+    expect(errorExists(err1, err1)).toBe(true)
+    expect(errorExists(err2, err1)).toBe(false)
+  })
+  it('should work for array', () => {
+    expect(errorExists(errs, err1)).toBe(true)
+    expect(errorExists(errs, err3)).toBe(false)
+  })
+})
--- src/base/error.mjs
+++ src/base/error.mjs
@@ -1,67 +1,29 @@
-import * as assert2 from "../base/assert2.mjs";
-
-const list = {};
-
-export function define(code, msg, field) {
-  assert2.e(list[code], undefined);
-  const ec = {
-    code: code,
-    message: msg
-  };
-  if (field) {
-    ec.field = field;
+export class FormError {
+  constructor(code, message, field) {
+    this.code = code
+    this.message = message
+    this.field = field
   }
-  list[code] = ec;
+
+  spawn() {
+    return new FormError(this.code, this.message, this.field)
+  }
 }
 
-export function get(code) {
-  return list[code];
-}
+export const UNKNOWN_ERROR = new FormError('UNKNOWN', 'Unknown error')
+export const INVALID_DATA = new FormError('INVALID_DATA', '비정상적인 값이 입력되었습니다.')
 
-define('INVALID_DATA', '비정상적인 값이 입력되었습니다.');
-define('INVALID_FORM', '*');
-
-const INVALID_FORM = get('INVALID_FORM');
-
-export function newError(obj) {
-  let err;
-  if (Array.isArray(obj)) {
-    err = new Error(INVALID_FORM.message);
-    err.code = INVALID_FORM.code;
-    err.errors = obj;
-    return err;
-  }
-  const ec = get(obj);
-  if (!ec) {
-    err = new Error('unknown error');
-    for (let p in obj) {
-      err[p] = obj[p];
-    }
-    return err;
-  }
-  if (ec.field) {
-    err = new Error(INVALID_FORM.message);
-    err.code = INVALID_FORM.code;
-    err.errors = [ec];
-    return err;
-  }
-  err = new Error(ec.message);
-  err.code = ec.code;
-  return err;
-}
-
-export function find(act, code) {
-  if (act.code === INVALID_FORM.code) {
-    for (let i = 0; i < act.errors.length; i++) {
-      const e = act.errors[i];
-      if (e.code === code) {
-        return true;
+export function errorExists(errs, err) {
+  if (errs instanceof Array) {
+    for (const e of errs) {
+      if (e.code === err.code) {
+        return true
       }
     }
   } else {
-    if (act.code === code) {
-      return true;
+    if (errs.code === err.code) {
+      return true
     }
   }
-  return false;
+  return false
 }
