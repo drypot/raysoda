cb0c61780a871f844876c5a7a2fa3726fb697ce7
drypot <drypot@gmail.com>
2021-08-10 23:31:04 +0900

    apply promise to about

--- src/app/about/about-test.ts
+++ src/app/about/about-test.ts
@@ -2,6 +2,7 @@ import { Express2 } from '../../lib/express/express2.js'
 import { Router } from 'express'
 import { SuperAgentTest } from 'supertest'
 import { loadConfig } from '../config/config.js'
+import { initAbout } from './about.js'
 
 describe('about pages', () => {
 
@@ -9,56 +10,42 @@ describe('about pages', () => {
   let router: Router
   let request: SuperAgentTest
 
-  beforeAll(done => {
+  beforeAll(async () => {
     const config = loadConfig('config/raysoda-test.json')
     server = new Express2(config)
     router = server.router
     request = server.spawnRequest()
-    server.start(done)
+    initAbout(server)
+    await server.start()
   })
 
-  afterAll(done => {
-    server.close(done)
+  afterAll(async () => {
+    await server.close()
   })
 
   describe('get /about/site', () => {
-    it('should work', done => {
-      request.get('/about/site').end(function (err, res) {
-        expect(err).toBeFalsy()
-        done()
-      })
+    it('should work', async () => {
+      await request.get('/about/site').expect(200)
     })
   })
   describe('get /about/company', () => {
-    it('should work', done => {
-      request.get('/about/company').end(function (err, res) {
-        expect(err).toBeFalsy()
-        done()
-      })
+    it('should work', async () => {
+      await request.get('/about/company').expect(200)
     })
   })
   describe('get /about/ad', () => {
-    it('should work', done => {
-      request.get('/about/ad').end(function (err, res) {
-        expect(err).toBeFalsy()
-        done()
-      })
+    it('should work', async () => {
+      await request.get('/about/ad').expect(200)
     })
   })
   describe('get /about/privacy', () => {
-    it('should work', done => {
-      request.get('/about/privacy').end(function (err, res) {
-        expect(err).toBeFalsy()
-        done()
-      })
+    it('should work', async () => {
+      await request.get('/about/privacy').expect(200)
     })
   })
   describe('get /about/help', () => {
-    it('should work', done => {
-      request.get('/about/help').end(function (err, res) {
-        expect(err).toBeFalsy()
-        done()
-      })
+    it('should work', async () => {
+      await request.get('/about/help').expect(200)
     })
   })
 
--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -4,23 +4,23 @@ export function initAbout(server: Express2) {
 
   const router = server.router
 
-  router.get('/about/site', function (req, res, done) {
+  router.get('/about/site', function (req, res) {
     res.render('app/about/view/about-site')
   })
 
-  router.get('/about/company', function (req, res, done) {
+  router.get('/about/company', function (req, res) {
     res.render('app/about/view/about-company')
   })
 
-  router.get('/about/ad', function (req, res, done) {
+  router.get('/about/ad', function (req, res) {
     res.render('app/about/view/about-ad')
   })
 
-  router.get('/about/privacy', function (req, res, done) {
+  router.get('/about/privacy', function (req, res) {
     res.render('app/about/view/about-privacy')
   })
 
-  router.get('/about/help', function (req, res, done) {
+  router.get('/about/help', function (req, res) {
     res.render('app/about/view/about-help')
   })
 
