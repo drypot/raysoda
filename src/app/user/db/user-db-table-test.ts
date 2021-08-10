d4e917fd567d2e60b360776ad874347d15d567c6
drypot <drypot@gmail.com>
2021-08-11 01:37:41 +0900

    update user-db-table-test to try to create index

--- src/app/user/db/user-db-table-test.ts
+++ src/app/user/db/user-db-table-test.ts
@@ -22,7 +22,7 @@ describe('UserDB', () => {
   describe('createTable', () => {
     it('should work', async () => {
       await udb.dropTable()
-      await udb.createTable(false)
+      await udb.createTable(true) // 인덱스까지 만들어 본다
       const r = await db.findTable('user')
       expect(r.length).toBe(1)
     })
