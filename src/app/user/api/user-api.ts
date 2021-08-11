6aeca4194e5e0e69d825861936c6a075dcac91ba
drypot <drypot@gmail.com>
2021-08-12 01:22:03 +0900

    move getUserForm to user-api

--- src/app/user/api/user-api.ts
+++ src/app/user/api/user-api.ts
@@ -1,11 +1,15 @@
 import { UserDB } from '../db/user-db.js'
 import { Express2, toCallback } from '../../../lib/express/express2.js'
-import { getUserForm } from '../form/user-form.js'
 import { addUserService } from '../service/user-service.js'
+import { Request } from 'express'
+import { UserForm } from '../form/user-form.js'
+import { UserCache } from '../cache/user-cache.js'
 
-export function initUserAPI(userdb: UserDB, web: Express2) {
+export function initUserAPI(udb: UserDB, uc: UserCache, web: Express2) {
 
   const router = web.router
+
+  // Register
 
   router.get('/user/register', (req, res) => {
     res.render('app/user/view/user-register')
@@ -15,11 +19,23 @@ export function initUserAPI(userdb: UserDB, web: Express2) {
     res.render('app/user/view/user-register-done')
   })
 
-  router.post('/api/user', toCallback(async function (req, res) {
+  router.post('/api/user', toCallback(async (req, res) => {
     let form = getUserForm(req)
     form.home = form.name
-    const user = await addUserService(userdb, form)
+    const user = await addUserService(udb, form)
     res.json({ id: user.id })
   }))
+
+  function getUserForm(req: Request): UserForm {
+    const body = req.body
+    return {
+      id: 0,
+      name: String(body.name ?? '').trim(),
+      home: String(body.home ?? '').trim(),
+      email: String(body.email ?? '').trim(),
+      password: String(body.password ?? '').trim(),
+      profile: String(body.profile ?? '').trim(),
+    }
+  }
 
 }
