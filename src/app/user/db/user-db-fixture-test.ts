7edf5cd626b1b0f22c078305bc879dd47a95e167
drypot <drypot@gmail.com>
2021-08-12 18:23:01 +0900

    add insertUserDBFixture4

---
+++ src/app/user/db/user-db-fixture-test.ts
@@ -0,0 +1,71 @@
+import { Config, loadConfig } from '../../config/config.js'
+import { DB } from '../../../lib/db/db.js'
+import { UserDB } from './user-db.js'
+import { newUser, User } from '../entity/user-entity.js'
+import { insertUserDBFixture1, insertUserDBFixture4 } from './user-db-fixture.js'
+
+describe('UserDB', () => {
+
+  let config: Config
+  let db: DB
+  let udb: UserDB
+
+  beforeAll(async () => {
+    config = loadConfig('config/app-test.json')
+    db = new DB(config)
+    udb = new UserDB(db)
+    await db.createDatabase()
+  })
+
+  afterAll(async () => {
+    await db.close()
+  })
+
+  describe('insertUserDBFixture1', () => {
+    beforeAll(async () => {
+      await udb.dropTable()
+      await udb.createTable(false)
+      await insertUserDBFixture1(udb)
+    })
+    it('user1 should exist', async () => {
+      let user = await udb.findUserById(1)
+      if (!user) throw new Error("User is empty")
+      expect(user.id).toBe(1)
+      expect(user.name).toBe('User Name 1')
+      expect(user.home).toBe('user1')
+    })
+  })
+
+  describe('insertUserDBFixture4', () => {
+    beforeAll(async () => {
+      await udb.dropTable()
+      await udb.createTable(false)
+      await insertUserDBFixture4(udb)
+    })
+    it('user1 should exist', async () => {
+      let user = await udb.findUserById(1)
+      if (!user) throw new Error("User is empty")
+      expect(user.id).toBe(1)
+      expect(user.name).toBe('User Name 1')
+      expect(user.home).toBe('user1')
+      expect(user.admin).toBe(false)
+    })
+    it('user3 should exist', async () => {
+      let user = await udb.findUserById(3)
+      if (!user) throw new Error("User is empty")
+      expect(user.id).toBe(3)
+      expect(user.name).toBe('User Name 3')
+      expect(user.home).toBe('user3')
+      expect(user.admin).toBe(false)
+    })
+    it('admin should exist', async () => {
+      let user = await udb.findUserById(4)
+      if (!user) throw new Error("User is empty")
+      expect(user.id).toBe(4)
+      expect(user.name).toBe('Admin Name')
+      expect(user.home).toBe('admin')
+      expect(user.admin).toBe(true)
+    })
+  })
+
+})
--- src/app/user/db/user-db-fixture.ts
+++ src/app/user/db/user-db-fixture.ts
@@ -1,12 +1,42 @@
-import { newUser } from '../entity/user-entity.js'
+import { newUser, User } from '../entity/user-entity.js'
 import { UserDB } from './user-db.js'
 
-export async function insertUserDBFixture(udb: UserDB): Promise<void> {
-  const users = [
-    newUser({ id: 1, name: 'Alice Liddell', home: 'alice', email: 'alice@mail.test' }),
-  ]
+const hash1 = '$2a$10$bP0BJpAzAUNFZ2Ejo57Gruhg8LkIQ9./EvfUjjyYqwCf3ZDhQZC1K' /* 1234 */
+
+const users1 = [
+  newUser({ id: 1, name: 'User Name 1', home: 'user1', email: 'user1@mail.test', hash: hash1 }),
+]
+
+const users4 = [
+  newUser({
+    id: 1, name: 'User Name 1', home: 'user1', email: 'user1@mail.test', hash: hash1,
+    pdate: new Date(2019, 0, 10)
+  }),
+  newUser({
+    id: 2, name: 'User Name 2', home: 'user2', email: 'user2@mail.test', hash: hash1,
+    pdate: new Date(2019, 0, 20)
+  }),
+  newUser({
+    id: 3, name: 'User Name 3', home: 'user3', email: 'user3@mail.test', hash: hash1,
+    pdate: new Date(2019, 0, 15)
+  }),
+  newUser({
+    id: 4, name: 'Admin Name', home: 'admin', email: 'admin@mail.test', hash: hash1,
+    pdate: new Date(2019, 0, 5), admin: true
+  })
+]
+
+async function insertUserDBFixture(udb: UserDB, users: User[]): Promise<void> {
   for (const user of users) {
     await udb.insertUser(user)
   }
-  udb.setNextUserId(2)
+  udb.setNextUserId(users.length + 1)
+}
+
+export async function insertUserDBFixture1(udb: UserDB): Promise<void> {
+  return insertUserDBFixture(udb, users1)
+}
+
+export async function insertUserDBFixture4(udb: UserDB): Promise<void> {
+  return insertUserDBFixture(udb, users4)
 }
