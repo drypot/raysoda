cce8221e852bddb4fb695bf29c8c0b2a14a3fdca
drypot <drypot@gmail.com>
2021-08-12 22:58:16 +0900

    add UserFix, UserFixAdmin

--- src/app/user/db/user-db-fixture-test.ts
+++ src/app/user/db/user-db-fixture-test.ts
@@ -1,7 +1,7 @@
 import { Config, loadConfig } from '../../config/config.js'
 import { DB } from '../../../lib/db/db.js'
 import { MSG_USER_UNDEFINED, UserDB } from './user-db.js'
-import { insertUserDBFixture1, insertUserDBFixture4 } from './user-db-fixture.js'
+import { insertUserDBFixture1, insertUserDBFixture4, UserFix1, UserFix3, UserFix4Admin } from './user-db-fixture.js'
 
 describe('UserDB', () => {
 
@@ -29,9 +29,8 @@ describe('UserDB', () => {
     it('user1 should exist', async () => {
       let user = await udb.findUserById(1)
       if (!user) throw new Error(MSG_USER_UNDEFINED)
-      expect(user.id).toBe(1)
-      expect(user.name).toBe('User Name 1')
-      expect(user.home).toBe('user1')
+      expect(user.name).toBe(UserFix1.name)
+      expect(user.home).toBe(UserFix1.home)
     })
   })
 
@@ -44,25 +43,22 @@ describe('UserDB', () => {
     it('user1 should exist', async () => {
       let user = await udb.findUserById(1)
       if (!user) throw new Error(MSG_USER_UNDEFINED)
-      expect(user.id).toBe(1)
-      expect(user.name).toBe('User Name 1')
-      expect(user.home).toBe('user1')
+      expect(user.name).toBe(UserFix1.name)
+      expect(user.home).toBe(UserFix1.home)
       expect(user.admin).toBe(false)
     })
     it('user3 should exist', async () => {
       let user = await udb.findUserById(3)
       if (!user) throw new Error(MSG_USER_UNDEFINED)
-      expect(user.id).toBe(3)
-      expect(user.name).toBe('User Name 3')
-      expect(user.home).toBe('user3')
+      expect(user.name).toBe(UserFix3.name)
+      expect(user.home).toBe(UserFix3.home)
       expect(user.admin).toBe(false)
     })
     it('admin should exist', async () => {
       let user = await udb.findUserById(4)
       if (!user) throw new Error(MSG_USER_UNDEFINED)
-      expect(user.id).toBe(4)
-      expect(user.name).toBe('Admin Name')
-      expect(user.home).toBe('admin')
+      expect(user.name).toBe(UserFix4Admin.name)
+      expect(user.home).toBe(UserFix4Admin.home)
       expect(user.admin).toBe(true)
     })
   })
--- src/app/user/db/user-db-fixture.ts
+++ src/app/user/db/user-db-fixture.ts
@@ -3,40 +3,39 @@ import { UserDB } from './user-db.js'
 
 const hash1 = '$2a$10$bP0BJpAzAUNFZ2Ejo57Gruhg8LkIQ9./EvfUjjyYqwCf3ZDhQZC1K' /* 1234 */
 
-const users1 = [
-  newUser({ id: 1, name: 'User Name 1', home: 'user1', email: 'user1@mail.test', hash: hash1 }),
-]
+export const UserFix1 = newUser({
+  id: 1, name: 'User Name 1', home: 'user1', email: 'user1@mail.test', hash: hash1,
+  pdate: new Date(2019, 0, 10)
+})
 
-const users4 = [
-  newUser({
-    id: 1, name: 'User Name 1', home: 'user1', email: 'user1@mail.test', hash: hash1,
-    pdate: new Date(2019, 0, 10)
-  }),
-  newUser({
+export const UserFix2 = newUser({
     id: 2, name: 'User Name 2', home: 'user2', email: 'user2@mail.test', hash: hash1,
     pdate: new Date(2019, 0, 20)
-  }),
-  newUser({
+  })
+
+export const UserFix3 = newUser({
     id: 3, name: 'User Name 3', home: 'user3', email: 'user3@mail.test', hash: hash1,
     pdate: new Date(2019, 0, 15)
-  }),
-  newUser({
+  })
+
+export const UserFix4Admin = newUser({
     id: 4, name: 'Admin Name', home: 'admin', email: 'admin@mail.test', hash: hash1,
     pdate: new Date(2019, 0, 5), admin: true
   })
-]
 
-async function insertUserDBFixture(udb: UserDB, users: User[]): Promise<void> {
+
+export async function insertUserDBFixture1(udb: UserDB): Promise<void> {
+  const users = [ UserFix1 ]
   for (const user of users) {
     await udb.insertUser(user)
   }
-  udb.setNextUserId(users.length + 1)
-}
-
-export async function insertUserDBFixture1(udb: UserDB): Promise<void> {
-  return insertUserDBFixture(udb, users1)
+  udb.setNextUserId(2)
 }
 
 export async function insertUserDBFixture4(udb: UserDB): Promise<void> {
-  return insertUserDBFixture(udb, users4)
+  const users = [ UserFix1, UserFix2, UserFix3, UserFix4Admin ]
+  for (const user of users) {
+    await udb.insertUser(user)
+  }
+  udb.setNextUserId(5)
 }
