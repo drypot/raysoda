48e38befa1d59e0c498d40c1703c87a35e1b7834
drypot <drypot@gmail.com>
2021-08-16 18:14:31 +0900

    change user/service to use factory

--- src/app/user/service/user-service-register-test.ts
+++ src/app/user/service/user-service-register-test.ts
@@ -1,4 +1,4 @@
-import { Config, loadConfig } from '../../config/config.js'
+import { Config, configFrom } from '../../config/config.js'
 import { DB } from '../../../lib/db/db.js'
 import { MSG_USER_NOT_FOUND, UserDB } from '../db/user-db.js'
 import {
@@ -8,13 +8,13 @@ import {
   NAME_DUPE,
   NAME_EMPTY,
   NAME_RANGE,
-  newUserForm,
+  userFormOf,
   PASSWORD_RANGE
 } from '../form/user-form.js'
-import { insertUserDBFixture1 } from '../db/user-db-fixture.js'
+import { insertUserFix1 } from '../db/user-db-fixture.js'
 import { FormError } from '../../../lib/base/error2.js'
 import { registerUser } from './user-service.js'
-import { checkPasswordHash } from '../entity/user-password.js'
+import { checkHash } from '../../../lib/base/hash.js'
 
 describe('UserService', () => {
 
@@ -23,90 +23,92 @@ describe('UserService', () => {
   let udb: UserDB
 
   beforeAll(async () => {
-    config = loadConfig('config/app-test.json')
-    db = new DB(config)
-    udb = new UserDB(db)
-    await db.createDatabase()
+    config = configFrom('config/app-test.json')
+    db = await DB.from(config).createDatabase()
+    udb = UserDB.from(db)
   })
 
   afterAll(async () => {
     await db.close()
   })
 
-  beforeAll(async () => {
-    await udb.dropTable()
-    await udb.createTable(false)
-    await insertUserDBFixture1(udb)
-  })
-
   describe('registerUser', () => {
-    it('should work when form valid', async () => {
-      const form = newUserForm({
+    it('init table', async () => {
+      await udb.dropTable()
+      await udb.createTable(false)
+    })
+    it('fill fix', async () => {
+      await insertUserFix1(udb)
+    })
+    it('ok if form valid', async () => {
+      const form = userFormOf({
         name: 'User X', home: 'userx', email: 'userx@mail.test', password: '1234', profile: '',
       })
-      const errs = [] as FormError[]
-      let user = await registerUser(udb, form, errs)
+      const errs: FormError[] = []
+      const user = await registerUser(udb, form, errs)
       if (!user) throw new Error(MSG_USER_NOT_FOUND)
-      const user2 = await udb.findUserById(user.id)
-      if (!user2) throw new Error(MSG_USER_NOT_FOUND)
-      expect(user2.name).toBe('User X')
-      expect(user2.home).toBe('userx')
-      expect(user2.email).toBe('userx@mail.test')
-      expect(await checkPasswordHash('1234', user2.hash)).toBe(true)
-      expect(user2.status).toBe('v')
-      expect(user2.admin).toBe(false)
     })
-    it('should fail when name is empty', async () => {
-      const form = newUserForm({ name: '', })
-      const errs = [] as FormError[]
+    it('can be checked', async () => {
+      const user = await udb.findUserByHome('userx')
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
+      expect(user.name).toBe('User X')
+      expect(user.home).toBe('userx')
+      expect(user.email).toBe('userx@mail.test')
+      expect(await checkHash('1234', user.hash)).toBe(true)
+      expect(user.status).toBe('v')
+      expect(user.admin).toBe(false)
+    })
+    it('fail if name empty', async () => {
+      const form = userFormOf({ name: '', })
+      const errs: FormError[] = []
       await registerUser(udb, form, errs)
       expect(errs).toContain(NAME_EMPTY)
     })
-    it('should fail when name is long', async () => {
-      const form = newUserForm({ name: 'x'.repeat(33), })
-      const errs = [] as FormError[]
+    it('fail if name is long', async () => {
+      const form = userFormOf({ name: 'x'.repeat(33), })
+      const errs: FormError[] = []
       await registerUser(udb, form, errs)
       expect(errs).toContain(NAME_RANGE)
     })
-    it('should fail when name is in use', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ name: 'User 1', })
+    it('fail if in use', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ name: 'User 1', })
       await registerUser(udb, form, errs)
       expect(errs).toContain(NAME_DUPE)
     })
-    it('should fail when name is in use 2', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ name: 'user1', })
+    it('fail if in use 2', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ name: 'user1', })
       await registerUser(udb, form, errs)
       expect(errs).toContain(NAME_DUPE)
     })
-    it('should fail when email is empty', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ email: '', })
+    it('fail if email empty', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ email: '', })
       await registerUser(udb, form, errs)
       expect(errs).toContain(EMAIL_EMPTY)
     })
-    it('should fail when email is invalid', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ email: 'abc.mail.test', })
+    it('fail if email format invalid', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ email: 'abc.mail.test', })
       await registerUser(udb, form, errs)
       expect(errs).toContain(EMAIL_PATTERN)
     })
-    it('should fail when email is in use', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ email: 'user1@mail.test', })
+    it('fail if email in use', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ email: 'user1@mail.test', })
       await registerUser(udb, form, errs)
       expect(errs).toContain(EMAIL_DUPE)
     })
-    it('should fail when password is short', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ password: '123', })
+    it('fail if password short', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ password: '123', })
       await registerUser(udb, form, errs)
       expect(errs).toContain(PASSWORD_RANGE)
     })
-    it('should fail when password is long', async () => {
-      const errs = [] as FormError[]
-      const form = newUserForm({ password: 'x'.repeat(33), })
+    it('fail if password long', async () => {
+      const errs: FormError[] = []
+      const form = userFormOf({ password: 'x'.repeat(33), })
       await registerUser(udb, form, errs)
       expect(errs).toContain(PASSWORD_RANGE)
     })
--- src/app/user/service/user-service.ts
+++ src/app/user/service/user-service.ts
@@ -1,43 +1,43 @@
 import {
   checkEmailFormat,
-  checkEmailUsable,
+  checkEmailDB,
   checkHomeFormat,
-  checkHomeUsable,
+  checkHomeDB,
   checkNameFormat,
-  checkNameUsable,
+  checkNameDB,
   checkPasswordFormat, USER_NOT_FOUND,
   UserForm
 } from '../form/user-form.js'
-import { newUser } from '../entity/user-entity.js'
+import { userOf } from '../entity/user-entity.js'
 import { FormError } from '../../../lib/base/error2.js'
 import { UserDB } from '../db/user-db.js'
-import { makePasswordHash } from '../entity/user-password.js'
+import { makeHash } from '../../../lib/base/hash.js'
 
 export async function registerUser(udb: UserDB, form: UserForm, errs: FormError[]) {
   checkNameFormat(form.name, errs)
   checkHomeFormat(form.home, errs)
   checkEmailFormat(form.email, errs)
   checkPasswordFormat(form.password, errs)
-  await checkNameUsable(udb, 0, form.name, errs)
-  await checkHomeUsable(udb, 0, form.home, errs)
-  await checkEmailUsable(udb, 0, form.email, errs)
+  await checkNameDB(udb, 0, form.name, errs)
+  await checkHomeDB(udb, 0, form.home, errs)
+  await checkEmailDB(udb, 0, form.email, errs)
   if (errs.length > 0) return
-  const user = newUser()
+  const user = userOf()
   user.id = udb.getNextUserId()
   user.name = form.name
   user.home = form.home
   user.email = form.email
   user.profile = form.profile
-  user.hash = await makePasswordHash(form.password)
+  user.hash = await makeHash(form.password)
   await udb.insertUser(user)
   return user
 }
 
 export async function deactivateUser(udb: UserDB, id: number, errs: FormError[]) {
-  const r = await udb.deactivateUser(id)
-  if (!r.changedRows) {
+  const count = await udb.deactivateUser(id)
+  if (!count) {
     errs.push(USER_NOT_FOUND)
     return
   }
-  udb.deleteCache(id)
+  udb.deleteCacheById(id)
 }
