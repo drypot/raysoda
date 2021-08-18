2eb357dbef413aa0e0314e3cb97eb09f794e2fcd
drypot <drypot@gmail.com>
2021-08-18 19:24:04 +0900

    update user/entity test message

--- src/app/user/entity/user-entity-test.ts
+++ src/app/user/entity/user-entity-test.ts
@@ -1,28 +1,29 @@
-import { userOf, User } from './user-entity.js'
+import { User, userOf } from './user-entity.js'
 
-describe('User', () => {
+describe('User Entity', () => {
 
-  it('can be created', () => {
-    const user = userOf()
-    expect(user).toBeDefined()
-    expect(user.id).toBe(0)
-    expect(user.name).toBe('')
-    expect(user.email).toBe('')
-    expect(user.status).toBe('v')
-  })
-
-  it('can be created 2', () => {
-    const _user: Partial<User> = {
-      id: 10,
-      name: 'User 1',
-      email: 'user1@mail.test',
-    }
-    const user = userOf(_user)
-    expect(user).toBeDefined()
-    expect(user.id).toBe(10)
-    expect(user.name).toBe('User 1')
-    expect(user.email).toBe('user1@mail.test')
-    expect(user.status).toBe('v')
+  describe('User', () => {
+    it('check create user', () => {
+      const user = userOf()
+      expect(user).toBeDefined()
+      expect(user.id).toBe(0)
+      expect(user.name).toBe('')
+      expect(user.email).toBe('')
+      expect(user.status).toBe('v')
+    })
+    it('check create user 2', () => {
+      const _user: Partial<User> = {
+        id: 10,
+        name: 'User 1',
+        email: 'user1@mail.test',
+      }
+      const user = userOf(_user)
+      expect(user).toBeDefined()
+      expect(user.id).toBe(10)
+      expect(user.name).toBe('User 1')
+      expect(user.email).toBe('user1@mail.test')
+      expect(user.status).toBe('v')
+    })
   })
 
 })
