eebc17e9e3b0d4fa02bb892130486c895d547dbd
drypot <drypot@gmail.com>
2021-08-18 18:35:35 +0900

    update lib/express test message

--- src/lib/express/express2-upload-test.ts
+++ src/lib/express/express2-upload-test.ts
@@ -3,8 +3,8 @@ import { deleteUpload, Express2 } from './express2.js'
 import { Router } from 'express'
 import { SuperAgentTest } from 'supertest'
 import { Multer } from 'multer'
-import { assertPathNotExists } from '../base/assert2.js'
 import { timeout } from '../base/async2.js'
+import { existsSync } from 'fs'
 
 describe('Express2 Upload', () => {
 
@@ -28,32 +28,28 @@ describe('Express2 Upload', () => {
     await web.close()
   })
 
-  beforeAll(() => {
-    router.post('/api/test/upload-file', upload.single('file'), deleteUpload(async (req, res) => {
-      res.json({
-        ...req.body,
-        file: req.file
-      })
-    }))
-    router.post('/api/test/upload-files', upload.array('files', 12), deleteUpload(async (req, res) => {
-      res.json({
-        ...req.body,
-        files: req.files
-      })
-    }))
-  })
-
-  describe('posting application/json', () => {
-    it('should work', async () => {
+  describe('upload handler', () => {
+    beforeAll(() => {
+      router.post('/api/test/upload-file', upload.single('file'), deleteUpload(async (req, res) => {
+        res.json({
+          ...req.body,
+          file: req.file
+        })
+      }))
+      router.post('/api/test/upload-files', upload.array('files', 12), deleteUpload(async (req, res) => {
+        res.json({
+          ...req.body,
+          files: req.files
+        })
+      }))
+    })
+    it('posting application/json should work', async () => {
       const res = await request.post('/api/test/upload-files').send({ 'p1': 'v1' })
       expect(res.body).toEqual({
         p1: 'v1'
       })
     })
-  })
-
-  describe('posting multipart/form-data field', () => {
-    it('should work', async () => {
+    it('posting multipart/form-data should work', async () => {
       const res = await request.post('/api/test/upload-files').field('p1', 'v1').field('p2', 'v2').field('p2', 'v3')
       expect(res.body).toEqual({
         p1: 'v1',
@@ -61,22 +57,15 @@ describe('Express2 Upload', () => {
         files: []
       })
     })
-    it('should work with object field', async () => {
-      const form = {
-        p1: 'v1',
-        p2: 'v2',
-        p3: ['v3', 'v4']
-      }
+    it('posting multipart/form-data should work 2', async () => {
+      const form = { p1: 'v1', p2: 'v2', p3: ['v3', 'v4'] }
       const res = await request.post('/api/test/upload-files').field(form)
       expect(res.body).toEqual({
         ...form,
         files: []
       })
     })
-  })
-
-  describe('post one file', () => {
-    it('should work', async () => {
+    it('posting one file should work', async () => {
       const res = await request.post('/api/test/upload-file').field('p1', 'v1').attach('file', f1)
       expect(res.body.err).toBeFalsy()
       expect(res.body.p1).toBe('v1')
@@ -93,12 +82,9 @@ describe('Express2 Upload', () => {
       //
       expect(res.body.file.originalname).toBe('express-upload-f1.txt')
       await timeout(100)
-      assertPathNotExists(res.body.file.path)
+      expect(existsSync(res.body.file.path)).toBe(false)
     })
-  })
-
-  describe('post two files', () => {
-    it('should work', async () => {
+    it('posting two files should work', async () => {
       const res = await request.post('/api/test/upload-files').field('p1', 'v1').attach('files', f1).attach('files', f2)
       expect(res.body.err).toBeFalsy()
       expect(res.body.p1).toBe('v1')
@@ -106,13 +92,10 @@ describe('Express2 Upload', () => {
       expect(res.body.files[0].originalname).toBe('express-upload-f1.txt')
       expect(res.body.files[1].originalname).toBe('express-upload-f2.txt')
       await timeout(100)
-      assertPathNotExists(res.body.files[0].path)
-      assertPathNotExists(res.body.files[1].path)
+      expect(existsSync(res.body.files[0].path)).toBe(false)
+      expect(existsSync(res.body.files[1].path)).toBe(false)
     })
-  })
-
-  describe('post file with irregular name', () => {
-    it('should work', async () => {
+    it('posting file with irregular name should work', async () => {
       const res = await request.post('/api/test/upload-files').attach('files', f1, 'file<>()[]_-=.txt.%$#@!&.txt')
       expect(res.body.err).toBeFalsy()
     })
