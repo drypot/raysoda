e4bb266ba4a2402fb358d36f55d99f06738eae0b
drypot <drypot@gmail.com>
2021-08-19 12:40:58 +0900

    fill user-update-service-test

--- src/app/user/case/update/user-update-api-test.ts
+++ src/app/user/case/update/user-update-api-test.ts
@@ -11,13 +11,14 @@ import { registerUserUpdateApi } from './user-update-api.js'
 import { checkHash } from '../../../../lib/base/hash.js'
 import {
   EMAIL_DUPE,
-  EMAIL_EMPTY,
+  EMAIL_RANGE,
   HOME_DUPE,
-  HOME_EMPTY,
+  HOME_RANGE,
   NAME_DUPE,
-  NAME_EMPTY,
+  NAME_RANGE,
   PASSWORD_RANGE
 } from '../register-form/user-form.js'
+import { FormError } from '../../../../lib/base/error2.js'
 
 describe('User Update Api', () => {
 
@@ -61,32 +62,6 @@ describe('User Update Api', () => {
     it('update user1', async () => {
       const form = {
         name: 'User X', home: 'userx', email: 'userx@mail.test',
-        password: '', profile: 'profile x'
-      }
-      const res = await request.put('/api/user/' + 1).send(form)
-      expect(res.body).toEqual({})
-    })
-    it('check db', async () => {
-      const user = await udb.findUserById(1)
-      if (!user) throw new Error(MSG_USER_NOT_FOUND)
-      expect(user.name).toBe('User X')
-      expect(user.home).toBe('userx')
-      expect(user.email).toBe('userx@mail.test')
-      expect(await checkHash('1234', user.hash)).toBe(true)
-      expect(user.profile).toBe('profile x')
-    })
-    it('check cache', async () => {
-      const user = await udb.getCachedById(1)
-      if (!user) throw new Error(MSG_USER_NOT_FOUND)
-      expect(user.name).toBe('User X')
-      expect(user.home).toBe('userx')
-      expect(user.email).toBe('userx@mail.test')
-      expect(await checkHash('1234', user.hash)).toBe(true)
-      expect(user.profile).toBe('profile x')
-    })
-    it('update user1 password', async () => {
-      const form = {
-        name: 'User X', home: 'userx', email: 'userx@mail.test',
         password: '5678', profile: 'profile x'
       }
       const res = await request.put('/api/user/' + 1).send(form)
@@ -95,57 +70,64 @@ describe('User Update Api', () => {
     it('check db', async () => {
       const user = await udb.findUserById(1)
       if (!user) throw new Error(MSG_USER_NOT_FOUND)
-      expect(await checkHash('1234', user.hash)).toBe(false)
+      expect(user.name).toBe('User X')
+      expect(user.home).toBe('userx')
+      expect(user.email).toBe('userx@mail.test')
+      expect(await checkHash('5678', user.hash)).toBe(true)
+      expect(user.profile).toBe('profile x')
+    })
+    it('check cache', async () => {
+      const user = await udb.getCachedById(1)
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
+      expect(user.name).toBe('User X')
+      expect(user.home).toBe('userx')
+      expect(user.email).toBe('userx@mail.test')
+      expect(await checkHash('5678', user.hash)).toBe(true)
+      expect(user.profile).toBe('profile x')
+    })
+    it('password can be omitted', async () => {
+      const form = {
+        name: 'User X', home: 'userx', email: 'userx@mail.test',
+        password: '', profile: 'profile x'
+      }
+      const res = await request.put('/api/user/' + 1).send(form)
+      expect(res.body).toEqual({})
+    })
+    it('check db', async () => {
+      const user = await udb.findUserById(1)
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
       expect(await checkHash('5678', user.hash)).toBe(true)
     })
     it('check cache', async () => {
       const user = await udb.getCachedById(1)
       if (!user) throw new Error(MSG_USER_NOT_FOUND)
-      expect(await checkHash('1234', user.hash)).toBe(false)
       expect(await checkHash('5678', user.hash)).toBe(true)
     })
-  })
-
-  describe('update user (format check)', () => {
-    it('init table', async () => {
-      await udb.dropTable()
-      await udb.createTable(false)
-    })
-    it('fill fix', async () => {
-      await insertUserFix4(udb)
-    })
-    it('login as user1', async () => {
-      await loginForTest(request, User1Login)
-    })
-    it('fails if name empty', async () => {
+    it('format check works', async () => {
+      const s33 = 'x'.repeat(33)
+      const s65 = 'x'.repeat(66)
       const form = {
-        name: '', home: '', email: '',
-        password: '', profile: ''
+        name: s33, home: s33, email: s65, password: s33, profile: ''
       }
       const res = await request.put('/api/user/' + 1).send(form)
-      expect(res.body.err.length).toBe(3)
-      expect(res.body.err).toContain(NAME_EMPTY)
-      expect(res.body.err).toContain(HOME_EMPTY)
-      expect(res.body.err).toContain(EMAIL_EMPTY)
+      const errs: FormError[] = res.body.err
+      expect(errs.length).toBe(4)
+      expect(errs).toContain(NAME_RANGE)
+      expect(errs).toContain(HOME_RANGE)
+      expect(errs).toContain(EMAIL_RANGE)
+      expect(errs).toContain(PASSWORD_RANGE)
     })
-    it('fails if name in use', async () => {
+    it('dupe check works', async () => {
       const form = {
         name: 'User 2', home: 'user2', email: 'user2@mail.test',
         password: '', profile: ''
       }
       const res = await request.put('/api/user/' + 1).send(form)
-      expect(res.body.err.length).toBe(3)
-      expect(res.body.err).toContain(NAME_DUPE)
-      expect(res.body.err).toContain(HOME_DUPE)
-      expect(res.body.err).toContain(EMAIL_DUPE)
-    })
-    it('fails if password short', async () => {
-      const form = {
-        name: 'User X', home: 'userx', email: 'userx@mail.test', password: '111'
-      }
-      const res = await request.put('/api/user/' + 1).send(form)
-      expect(res.body.err.length).toBe(1)
-      expect(res.body.err).toContain(PASSWORD_RANGE)
+      const errs: FormError[] = res.body.err
+      expect(errs.length).toBe(3)
+      expect(errs).toContain(NAME_DUPE)
+      expect(errs).toContain(HOME_DUPE)
+      expect(errs).toContain(EMAIL_DUPE)
     })
   })
 
--- src/app/user/case/update/user-update-service-test.ts
+++ src/app/user/case/update/user-update-service-test.ts
@@ -1,9 +1,21 @@
 import { Config, configFrom } from '../../../config/config.js'
 import { DB } from '../../../../lib/db/db.js'
-import { UserDB } from '../../db/user-db.js'
-import { insertUserFix1 } from '../../db/user-db-fixture.js'
+import { MSG_USER_NOT_FOUND, UserDB } from '../../db/user-db.js'
+import { insertUserFix4 } from '../../db/user-db-fixture.js'
+import { FormError } from '../../../../lib/base/error2.js'
+import { userUpdateService } from './user-update-service.js'
+import { checkHash } from '../../../../lib/base/hash.js'
+import {
+  EMAIL_DUPE,
+  EMAIL_RANGE,
+  HOME_DUPE,
+  HOME_RANGE,
+  NAME_DUPE,
+  NAME_RANGE,
+  PASSWORD_RANGE
+} from '../register-form/user-form.js'
 
-describe('UserUpdateService', () => {
+describe('User Update Service', () => {
 
   let config: Config
   let db: DB
@@ -19,15 +31,88 @@ describe('UserUpdateService', () => {
     await db.close()
   })
 
-  describe('userViewService', () => {
+  describe('userUpdateService', () => {
     it('init table', async () => {
       await udb.dropTable()
       await udb.createTable(false)
     })
     it('fill fix', async () => {
-      await insertUserFix1(udb)
+      await insertUserFix4(udb)
     })
-
+    it('update user1', async () => {
+      const form = {
+        name: 'User X', home: 'userx', email: 'userx@mail.test',
+        password: '', profile: 'profile x'
+      }
+      const errs: FormError[] = []
+      await userUpdateService(udb, 1, form, errs)
+      expect(errs.length).toBe(0)
+    })
+    it('check db', async () => {
+      const user = await udb.findUserById(1)
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
+      expect(user.name).toBe('User X')
+      expect(user.home).toBe('userx')
+      expect(user.email).toBe('userx@mail.test')
+      expect(await checkHash('1234', user.hash)).toBe(true)
+      expect(user.profile).toBe('profile x')
+    })
+    it('check cache', async () => {
+      const user = await udb.getCachedById(1)
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
+      expect(user.name).toBe('User X')
+      expect(user.home).toBe('userx')
+      expect(user.email).toBe('userx@mail.test')
+      expect(await checkHash('1234', user.hash)).toBe(true)
+      expect(user.profile).toBe('profile x')
+    })
+    it('update user1 password', async () => {
+      const form = {
+        name: 'User X', home: 'userx', email: 'userx@mail.test',
+        password: '5678', profile: 'profile x'
+      }
+      const errs: FormError[] = []
+      await userUpdateService(udb, 1, form, errs)
+      expect(errs.length).toBe(0)
+    })
+    it('check db', async () => {
+      const user = await udb.findUserById(1)
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
+      expect(await checkHash('1234', user.hash)).toBe(false)
+      expect(await checkHash('5678', user.hash)).toBe(true)
+    })
+    it('check cache', async () => {
+      const user = await udb.getCachedById(1)
+      if (!user) throw new Error(MSG_USER_NOT_FOUND)
+      expect(await checkHash('1234', user.hash)).toBe(false)
+      expect(await checkHash('5678', user.hash)).toBe(true)
+    })
+    it('format check works', async () => {
+      const s33 = 'x'.repeat(33)
+      const s65 = 'x'.repeat(66)
+      const form = {
+        name: s33, home: s33, email: s65, password: s33, profile: ''
+      }
+      const errs: FormError[] = []
+      await userUpdateService(udb, 1, form, errs)
+      expect(errs.length).toBe(4)
+      expect(errs).toContain(NAME_RANGE)
+      expect(errs).toContain(HOME_RANGE)
+      expect(errs).toContain(EMAIL_RANGE)
+      expect(errs).toContain(PASSWORD_RANGE)
+    })
+    it('dupe check works', async () => {
+      const form = {
+        name: 'User 2', home: 'user2', email: 'user2@mail.test',
+        password: '', profile: ''
+      }
+      const errs: FormError[] = []
+      await userUpdateService(udb, 1, form, errs)
+      expect(errs.length).toBe(3)
+      expect(errs).toContain(NAME_DUPE)
+      expect(errs).toContain(HOME_DUPE)
+      expect(errs).toContain(EMAIL_DUPE)
+    })
   })
 
 })
