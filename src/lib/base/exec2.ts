f6ea8f208e6df84d4c139024ab472dc92dbf6379
drypot <drypot@gmail.com>
2021-08-26 15:33:45 +0900

    update exec2

--- src/lib/base/exec2.ts
+++ src/lib/base/exec2.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process'
 
 export function exec2(cmd: string) {
-  return new Promise((resolve, reject) => {
+  return new Promise<string>((resolve, reject) => {
     exec(cmd, (err, stdout, stderr) => {
       if (err) return reject(err)
       resolve(stdout || stderr)
