dedd8bf4a500a0505dd01cd8ac2cd4d6d11dc1f9
drypot <drypot@gmail.com>
2021-08-26 15:33:29 +0900

    change fs2 to async

--- src/lib/base/fs2-test.ts
+++ src/lib/base/fs2-test.ts
@@ -1,19 +1,20 @@
-import { existsSync, writeFileSync } from 'fs'
-import { emptyDirSync, mkdirRecursiveSync, rmRecursiveSync } from './fs2.js'
+import { existsSync } from 'fs'
+import { emptyDir, mkdirRecursive, rmRecursive } from './fs2.js'
+import { writeFile } from 'fs/promises'
 
 describe('fs2', () => {
 
   const tdir = 'tmp/fs/'
 
   describe('rm/mkdir', () => {
-    it('rm root', () => {
-      rmRecursiveSync(tdir)
+    it('rm root', async () => {
+      await rmRecursive(tdir)
     })
     it('dir not exist', () => {
       expect(existsSync(tdir)).toBe(false)
     })
-    it('mkdir root', () => {
-      mkdirRecursiveSync(tdir)
+    it('mkdir root', async () => {
+      await mkdirRecursive(tdir)
     })
     it('dir not exist', () => {
       expect(existsSync(tdir)).toBe(true)
@@ -21,35 +22,35 @@ describe('fs2', () => {
     it('sub dir not exist', () => {
       expect(existsSync(tdir + '1/1/1')).toBe(false)
     })
-    it('mkdir sub', () => {
-      mkdirRecursiveSync(tdir + '1/1/1')
+    it('mkdir sub', async () => {
+      await mkdirRecursive(tdir + '1/1/1')
     })
     it('sub dir exist', () => {
       expect(existsSync(tdir + '1/1/1')).toBe(true)
     })
-    it('rm sub', () => {
-      rmRecursiveSync(tdir + '1/1/1')
+    it('rm sub', async () => {
+      await rmRecursive(tdir + '1/1/1')
     })
     it('sub dir not exist', () => {
       expect(existsSync(tdir + '1/1/1')).toBe(false)
       expect(existsSync(tdir + '1/1')).toBe(true)
     })
-    it('rm invalid dir', () => {
-      rmRecursiveSync(tdir + 'xxx')
+    it('rm invalid dir', async () => {
+      await rmRecursive(tdir + 'xxx')
     })
   })
 
   describe('empty dir', () => {
-    it('init files', () => {
-      rmRecursiveSync(tdir)
-      mkdirRecursiveSync(tdir + '1/2')
-      writeFileSync(tdir + '0.txt', 'abc')
-      writeFileSync(tdir + '1/11.txt', 'abc')
-      writeFileSync(tdir + '1/12.txt', 'abc')
-      writeFileSync(tdir + '1/2/2.txt', 'abc')
+    it('init files', async () => {
+      await rmRecursive(tdir)
+      await mkdirRecursive(tdir + '1/2')
+      await writeFile(tdir + '0.txt', 'abc')
+      await writeFile(tdir + '1/11.txt', 'abc')
+      await writeFile(tdir + '1/12.txt', 'abc')
+      await writeFile(tdir + '1/2/2.txt', 'abc')
     })
-    it('empty dir 1', () => {
-      emptyDirSync(tdir + '1')
+    it('empty dir 1', async () => {
+      await emptyDir(tdir + '1')
     })
     it('check', () => {
       expect(existsSync(tdir + '0.txt')).toBe(true)
@@ -57,8 +58,8 @@ describe('fs2', () => {
       expect(existsSync(tdir + '1/12.txt')).toBe(false)
       expect(existsSync(tdir + '1/2/2.txt')).toBe(true)
     })
-    it('empty invalid dir', () => {
-      emptyDirSync(tdir + 'xxx')
+    it('empty invalid dir', async () => {
+      await expectAsync(emptyDir(tdir + 'xxx')).toBeRejected()
     })
   })
 })
--- src/lib/base/fs2.ts
+++ src/lib/base/fs2.ts
@@ -1,21 +1,18 @@
-import { mkdirSync, readdirSync, rmSync, unlinkSync } from 'fs'
+import { mkdir, readdir, rm, unlink } from 'fs/promises'
 
-export function mkdirRecursiveSync(p: string) {
-  mkdirSync(p, { recursive: true, mode: 0o755, })
+export async function mkdirRecursive(p: string) {
+  return mkdir(p, { recursive: true, mode: 0o755, })
 }
 
-export function rmRecursiveSync(p: string) {
-  rmSync(p, { force: true, recursive: true })
+export async function rmRecursive(p: string) {
+  return rm(p, { force: true, recursive: true })
 }
 
-export function emptyDirSync(p: string) {
-  try {
-    const dirent = readdirSync(p, { withFileTypes: true })
-    for (const e of dirent) {
-      if (e.isFile()) {
-        unlinkSync(p + '/' + e.name)
-      }
+export async function emptyDir(p: string) {
+  const dirent = await readdir(p, { withFileTypes: true })
+  for (const e of dirent) {
+    if (e.isFile()) {
+      await unlink(p + '/' + e.name)
     }
-  } catch {
   }
 }
