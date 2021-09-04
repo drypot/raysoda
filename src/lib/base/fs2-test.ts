3d5f0c72e8266577ccd54250f09d3c5f7b71a8ba
drypot <drypot@gmail.com>
2021-09-04 20:07:46 +0900

    add fs2 sync functions

--- src/lib/base/fs2-test.ts
+++ src/lib/base/fs2-test.ts
@@ -1,5 +1,5 @@
-import { existsSync } from 'fs'
-import { emptyDir, mkdirRecursive, rmRecursive } from './fs2.js'
+import { existsSync, writeFileSync } from 'fs'
+import { emptyDir, emptyDirSync, mkdirRecursive, mkdirRecursiveSync, rmRecursive, rmRecursiveSync } from './fs2.js'
 import { writeFile } from 'fs/promises'
 
 // exists 는 deprecated 되었지만 existsSync 는 계속 유효하다.
@@ -45,6 +45,40 @@ describe('fs2', () => {
     })
   })
 
+  describe('rm/mkdir sync', () => {
+    it('rm root', () => {
+      rmRecursiveSync(tdir)
+    })
+    it('dir not exist', () => {
+      expect(existsSync(tdir)).toBe(false)
+    })
+    it('mkdir root', () => {
+      mkdirRecursiveSync(tdir)
+    })
+    it('dir not exist', () => {
+      expect(existsSync(tdir)).toBe(true)
+    })
+    it('sub dir not exist', () => {
+      expect(existsSync(tdir + '1/1/1')).toBe(false)
+    })
+    it('mkdir sub', () => {
+      mkdirRecursiveSync(tdir + '1/1/1')
+    })
+    it('sub dir exist', () => {
+      expect(existsSync(tdir + '1/1/1')).toBe(true)
+    })
+    it('rm sub', () => {
+      rmRecursiveSync(tdir + '1/1/1')
+    })
+    it('sub dir not exist', () => {
+      expect(existsSync(tdir + '1/1/1')).toBe(false)
+      expect(existsSync(tdir + '1/1')).toBe(true)
+    })
+    it('rm invalid dir', () => {
+      rmRecursiveSync(tdir + 'xxx')
+    })
+  })
+
   describe('empty dir', () => {
     it('init files', async () => {
       await rmRecursive(tdir)
@@ -65,6 +99,31 @@ describe('fs2', () => {
     })
     it('empty invalid dir', async () => {
       await expectAsync(emptyDir(tdir + 'xxx')).toBeRejected()
+    })
+  })
+
+  describe('empty dir sync', () => {
+    it('init files', () => {
+      rmRecursiveSync(tdir)
+      mkdirRecursiveSync(tdir + '1/2')
+      writeFileSync(tdir + '0.txt', 'abc')
+      writeFileSync(tdir + '1/11.txt', 'abc')
+      writeFileSync(tdir + '1/12.txt', 'abc')
+      writeFileSync(tdir + '1/2/2.txt', 'abc')
+    })
+    it('empty dir 1', () => {
+      emptyDirSync(tdir + '1')
+    })
+    it('check', () => {
+      expect(existsSync(tdir + '0.txt')).toBe(true)
+      expect(existsSync(tdir + '1/11.txt')).toBe(false)
+      expect(existsSync(tdir + '1/12.txt')).toBe(false)
+      expect(existsSync(tdir + '1/2/2.txt')).toBe(true)
+    })
+    it('empty invalid dir', () => {
+      expect(() => {
+        emptyDirSync(tdir + 'xxx')
+      }).toThrow()
     })
   })
 })
--- src/lib/base/fs2.ts
+++ src/lib/base/fs2.ts
@@ -1,11 +1,20 @@
 import { mkdir, readdir, rm, unlink } from 'fs/promises'
+import { mkdirSync, readdirSync, rmSync, unlinkSync } from 'fs'
 
 export async function mkdirRecursive(p: string) {
   await mkdir(p, { recursive: true, mode: 0o755, })
 }
 
+export function mkdirRecursiveSync(p: string) {
+  mkdirSync(p, { recursive: true, mode: 0o755, })
+}
+
 export async function rmRecursive(p: string) {
   await rm(p, { force: true, recursive: true })
+}
+
+export function rmRecursiveSync(p: string) {
+  rmSync(p, { force: true, recursive: true })
 }
 
 export async function emptyDir(p: string) {
@@ -13,6 +22,15 @@ export async function emptyDir(p: string) {
   for (const e of dirent) {
     if (e.isFile()) {
       await unlink(p + '/' + e.name)
+    }
+  }
+}
+
+export function emptyDirSync(p: string) {
+  const dirent = readdirSync(p, { withFileTypes: true })
+  for (const e of dirent) {
+    if (e.isFile()) {
+      unlinkSync(p + '/' + e.name)
     }
   }
 }
