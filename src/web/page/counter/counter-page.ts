bdfb2e9c61f9fb0fca4b977b9e485a6a6be67c93
drypot <drypot@gmail.com>
2021-10-01 00:40:29 +0900

    change counter-list to counter

--- src/web/page/counter/counter-page.ts
+++ src/web/page/counter/counter-page.ts
@@ -4,11 +4,11 @@ import { NOT_AUTHENTICATED, NOT_AUTHORIZED } from '../../../_type/error-user.js'
 
 export function registerCounterPage(web: Express2) {
 
-  web.router.get('/counter-list', toCallback(async (req, res) => {
+  web.router.get('/counter', toCallback(async (req, res) => {
     const user = sessionUserFrom(res)
     if (!user) throw NOT_AUTHENTICATED
     if (!user.admin) throw NOT_AUTHORIZED
-    res.render('counter/counter-list')
+    res.render('counter/counter')
   }))
 
 }
