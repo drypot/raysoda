43a16cc667157cecf7936801ff8e785f13fcaa68
drypot <drypot@gmail.com>
2021-10-01 01:30:20 +0900

    move user profile page redirect to redirect.ts

--- src/web/api/aux/redirect-test.ts
+++ src/web/api/aux/redirect-test.ts
@@ -21,28 +21,43 @@ describe('Redirect', () => {
     await web.close()
   })
 
-  describe('redirecting', () => {
-    it('/Com/Photo/View.aspx', async () => {
-      await request.get('/Com/Photo/View.aspx?f=A&pg=3&p=937928')
-        .expect(301).expect('Location', '/image/937928')
-    })
-    it('/Com/Photo/List.aspx', async () => {
-      await request.get('/Com/Photo/List.aspx?f=A&s=DD&pg=3')
-        .expect(301).expect('Location', '/')
-    })
+  it('/Com/Photo/View.aspx', async () => {
+    await request.get('/Com/Photo/View.aspx?f=A&pg=3&p=937928')
+      .expect(301).expect('Location', '/image/937928')
+  })
+  it('/Com/Photo/List.aspx', async () => {
+    await request.get('/Com/Photo/List.aspx?f=A&s=DD&pg=3')
+      .expect(301).expect('Location', '/')
+  })
 
-    it('/Com/Photo/CList.aspx', async () => {
-      await request.get('/Com/Photo/CList.aspx?f=C')
-        .expect(301).expect('Location', '/')
-    })
-    it('/users', async () => {
-      await request.get('/users/1')
-        .expect(301).expect('Location', '/user/1')
-    })
-    it('/images', async () => {
-      await request.get('/images/1')
-        .expect(301).expect('Location', '/image/1')
-    })
+  it('/Com/Photo/CList.aspx', async () => {
+    await request.get('/Com/Photo/CList.aspx?f=C')
+      .expect(301).expect('Location', '/')
+  })
+  it('/users', async () => {
+    await request.get('/users/1')
+      .expect(301).expect('Location', '/user/1')
+  })
+  it('/images', async () => {
+    await request.get('/images/1')
+      .expect(301).expect('Location', '/image/1')
+  })
+
+  it('/user1', async () => {
+    const res = await request.get('/user1').expect(301).expect('Location', '/user/user1')
+  })
+  it('/USER1', async () => {
+    const res = await request.get('/USER1').expect(301).expect('Location', '/user/USER1')
+  })
+  it('/xman', async () => {
+    const res = await request.get('/xman').expect(301).expect('Location', '/user/xman')
+  })
+  it('/xman/yman', async () => {
+    const res = await request.get('/xman/yman').expect(404)
+  })
+  it('/xman/yman encoded', async () => {
+    const res = await request.get('/' + encodeURIComponent('xman/yman'))
+      .expect(301).expect('Location', '/user/' + encodeURIComponent('xman/yman'))
   })
 
 })
--- src/web/api/aux/redirect.ts
+++ src/web/api/aux/redirect.ts
@@ -1,14 +1,12 @@
-import { Express2 } from '../../_express/express2.js'
+import { Express2, toCallback } from '../../_express/express2.js'
 
 export function registerRedirect(web: Express2) {
 
-  const router = web.router
-
-  router.get('/Com/Photo/View.aspx', function (req, res) {
+  web.router.get('/Com/Photo/View.aspx', function (req, res) {
     res.redirect(301, '/image/' + req.query.p)
   })
 
-  router.get('/Com/Photo/List.aspx', function (req, res) {
+  web.router.get('/Com/Photo/List.aspx', function (req, res) {
     if (req.query.u) {
       res.redirect(301, '/user/' + req.query.u)
     } else {
@@ -16,16 +14,19 @@ export function registerRedirect(web: Express2) {
     }
   })
 
-  router.get('/Com/Photo/CList.aspx', function (req, res) {
+  web.router.get('/Com/Photo/CList.aspx', function (req, res) {
     res.redirect(301, '/')
   })
 
-  router.get('/users/:id([0-9]+)', function (req, res) {
+  web.router.get('/users/:id([0-9]+)', function (req, res) {
     res.redirect(301, '/user/' + req.params.id)
   })
 
-  router.get('/images/:id([0-9]+)', function (req, res) {
+  web.router.get('/images/:id([0-9]+)', function (req, res) {
     res.redirect(301, '/image/' + req.params.id)
   })
 
+  web.router.get('/:name([^/]+)', toCallback(async (req, res) => {
+    res.redirect(301, '/user/' + encodeURIComponent(req.params.name))
+  }))
 }
