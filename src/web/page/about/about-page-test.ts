54d90abf37bbf50187631742eca65b373f6d0172
drypot <drypot@gmail.com>
2021-10-04 16:12:08 +0900

    update web/page/about

--- src/web/page/about/about-page-test.ts
+++ src/web/page/about/about-page-test.ts
@@ -1,41 +1,39 @@
 import { Express2 } from '../../_express/express2.js'
-import { SuperAgentTest } from 'supertest'
-import { readConfigSync } from '../../../_util/config-loader.js'
+import supertest, { SuperAgentTest } from 'supertest'
+import { loadConfigSync } from '../../../_util/config-loader.js'
 import { registerAboutPage } from './about-page.js'
 
-describe('About Page', () => {
+describe('AboutPage', () => {
 
-  let server: Express2
-  let request: SuperAgentTest
+  let web: Express2
+  let sat: SuperAgentTest
 
   beforeAll(async () => {
-    const config = readConfigSync('config/raysoda-test.json')
-    server = Express2.from(config)
-    registerAboutPage(server)
-    await server.start()
-    request = server.spawnRequest()
+    const config = loadConfigSync('config/raysoda-test.json')
+    web = Express2.from(config)
+    registerAboutPage(web)
+    await web.start()
+    sat = supertest.agent(web.server)
   })
 
   afterAll(async () => {
-    await server.close()
+    await web.close()
   })
 
-  describe('about pages', () => {
-    it('/about', async () => {
-      await request.get('/about').expect(200).expect(/<title>About/)
-    })
-    it('/company', async () => {
-      await request.get('/company').expect(200).expect(/<title>Company/)
-    })
-    it('/ad', async () => {
-      await request.get('/ad').expect(200).expect(/<title>Advertise/)
-    })
-    it('/privacy', async () => {
-      await request.get('/privacy').expect(200).expect(/<title>Privacy/)
-    })
-    it('/help', async () => {
-      await request.get('/help').expect(200).expect(/<title>Help/)
-    })
+  it('/about', async () => {
+    await sat.get('/about').expect(200).expect(/<title>About/)
+  })
+  it('/company', async () => {
+    await sat.get('/company').expect(200).expect(/<title>Company/)
+  })
+  it('/ad', async () => {
+    await sat.get('/ad').expect(200).expect(/<title>Advertise/)
+  })
+  it('/privacy', async () => {
+    await sat.get('/privacy').expect(200).expect(/<title>Privacy/)
+  })
+  it('/help', async () => {
+    await sat.get('/help').expect(200).expect(/<title>Help/)
   })
 
 })
--- src/web/page/about/about-page.ts
+++ src/web/page/about/about-page.ts
@@ -1,26 +1,24 @@
 import { Express2 } from '../../_express/express2.js'
 
-export function registerAboutPage(server: Express2) {
+export function registerAboutPage(web: Express2) {
 
-  const router = server.router
-
-  router.get('/about', function (req, res) {
+  web.router.get('/about', function (req, res) {
     res.render('about/about')
   })
 
-  router.get('/company', function (req, res) {
+  web.router.get('/company', function (req, res) {
     res.render('about/company')
   })
 
-  router.get('/ad', function (req, res) {
+  web.router.get('/ad', function (req, res) {
     res.render('about/ad')
   })
 
-  router.get('/privacy', function (req, res) {
+  web.router.get('/privacy', function (req, res) {
     res.render('about/privacy')
   })
 
-  router.get('/help', function (req, res) {
+  web.router.get('/help', function (req, res) {
     res.render('about/help')
   })
 
