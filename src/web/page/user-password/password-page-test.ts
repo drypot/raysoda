4e8773c16acf7544db10a6db22f84bc3e6f71d92
drypot <drypot@gmail.com>
2021-10-04 16:20:10 +0900

    update web/page/user-password

--- src/web/page/user-password/password-page-test.ts
+++ src/web/page/user-password/password-page-test.ts
@@ -1,37 +1,36 @@
-import { readConfigSync } from '../../../_util/config-loader.js'
+import { loadConfigSync } from '../../../_util/config-loader.js'
 import { Express2 } from '../../_express/express2.js'
-import { SuperAgentTest } from 'supertest'
+import supertest, { SuperAgentTest } from 'supertest'
 import { registerPasswordPage } from './password-page.js'
 import { Config } from '../../../_type/config.js'
 
-describe('Password Page', () => {
+describe('PasswordPage', () => {
 
   let config: Config
 
   let web: Express2
-  let request: SuperAgentTest
+  let sat: SuperAgentTest
 
   beforeAll(async () => {
-    config = readConfigSync('config/app-test.json')
-    web = await Express2.from(config).start()
+    config = loadConfigSync('config/app-test.json')
+    web = Express2.from(config)
     registerPasswordPage(web)
-    request = web.spawnRequest()
+    await web.start()
+    sat = supertest.agent(web.server)
   })
 
   afterAll(async () => {
     await web.close()
   })
 
-  describe('password reset pages ', () => {
-    it('user-password-reset', async () => {
-      await request.get('/password-reset').expect(200).expect(/<title>Password/)
-    })
-    it('user-password-reset-2', async () => {
-      await request.get('/password-reset-2').expect(200).expect(/<title>Password/)
-    })
-    it('/password-reset-3', async () => {
-      await request.get('/password-reset-3').expect(200).expect(/<title>Password/)
-    })
+  it('user-password-reset', async () => {
+    await sat.get('/password-reset').expect(200).expect(/<title>Password/)
+  })
+  it('user-password-reset-2', async () => {
+    await sat.get('/password-reset-2').expect(200).expect(/<title>Password/)
+  })
+  it('/password-reset-3', async () => {
+    await sat.get('/password-reset-3').expect(200).expect(/<title>Password/)
   })
 
 })
