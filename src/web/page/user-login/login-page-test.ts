be266552b9d30968e7eed5e2f850930f5a5c74ed
drypot <drypot@gmail.com>
2021-10-06 01:23:47 +0900

    add logout page

--- src/web/page/user-login/login-page-test.ts
+++ src/web/page/user-login/login-page-test.ts
@@ -3,17 +3,32 @@ import { Express2 } from '../../_express/express2.js'
 import supertest, { SuperAgentTest } from 'supertest'
 import { registerLoginPage } from './login-page.js'
 import { Config } from '../../../_type/config.js'
+import { DB } from '../../../db/_db/db.js'
+import { UserDB } from '../../../db/user/user-db.js'
+import { UserCache } from '../../../db/user/cache/user-cache.js'
+import { registerLoginApi } from '../../api/user-login/login-api.js'
+import { insertUserFix4, USER1_LOGIN } from '../../../db/user/fixture/user-fix.js'
+import { NOT_AUTHENTICATED } from '../../../_type/error-user.js'
+import { loginForTest } from '../../api/user-login/login-api-fixture.js'
 
 describe('LoginPage', () => {
 
   let config: Config
-
+  let db: DB
+  let udb: UserDB
+  let uc: UserCache
   let web: Express2
   let sat: SuperAgentTest
 
   beforeAll(async () => {
     config = loadConfigSync('config/app-test.json')
+
+    db = await DB.from(config).createDatabase()
+    udb = UserDB.from(db)
+    uc = UserCache.from(udb)
+
     web = Express2.from(config)
+    registerLoginApi(web, uc)
     registerLoginPage(web)
     await web.start()
     sat = supertest.agent(web.server)
@@ -21,10 +36,34 @@ describe('LoginPage', () => {
 
   afterAll(async () => {
     await web.close()
+    await db.close()
+  })
+
+  it('init table', async () => {
+    await udb.dropTable()
+    await udb.createTable(false)
+  })
+  it('fill fix', async () => {
+    await insertUserFix4(udb)
   })
 
   it('login', async () => {
     await sat.get('/login').expect(200).expect(/<title>Login/)
+  })
+
+  it('login as user1', async () => {
+    await loginForTest(sat, USER1_LOGIN)
+  })
+  it('login-info 1', async () => {
+    const res = await sat.get('/api/login-info').expect(200)
+    expect(res.body.err).toBeUndefined()
+  })
+  it('logout', async () => {
+    await sat.get('/logout').expect(302).expect('Location', '/')
+  })
+  it('login-info fails', async () => {
+    const res = await sat.get('/api/login-info').expect(200)
+    expect(res.body.err).toContain(NOT_AUTHENTICATED)
   })
 
 })
--- src/web/page/user-login/login-page.ts
+++ src/web/page/user-login/login-page.ts
@@ -1,9 +1,15 @@
-import { Express2 } from '../../_express/express2.js'
+import { Express2, toCallback } from '../../_express/express2.js'
+import { logoutService } from '../../api/user-login/login-api.js'
 
 export function registerLoginPage(web: Express2) {
 
   web.router.get('/login', (req, res) => {
     res.render('user-login/login')
   })
+
+  web.router.get('/logout', toCallback(async (req, res) => {
+    await logoutService(req, res)
+    res.redirect('/')
+  }))
 
 }
