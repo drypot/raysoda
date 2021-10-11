63577b3ba35c60606c393504881b2ba5b26c28fb
drypot <drypot@gmail.com>
2021-10-11 20:47:31 +0900

    clean tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@ module.exports = {
   mode: 'jit',
   purge: {
     content: ['src/server/web/template/ejs/**/*.{ejs}'],
-    // extractors: [
-    //   {
-    //     extractor: require('purgecss-from-pug'),
-    //     extensions: ['pug']
-    //   }
-    // ]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
