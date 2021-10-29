8b5a46b6fdc22f3d9f65ca2ea04a91b4486fa0b7
drypot <drypot@gmail.com>
2021-10-29 22:56:37 +0900

    add emailGetUserName

--- src/ts/common/util/email-test.ts
+++ src/ts/common/util/email-test.ts
@@ -1,19 +1,40 @@
-import { emailPatternIsOk } from './email'
+import { emailGetUserName, emailPatternIsOk } from './email'
 
 describe('emailPatternIsOk', () => {
-  it('correct mail', () => {
+  it('normal ok', () => {
     expect(emailPatternIsOk('drypot@mail.test')).toBe(true)
   })
-  it('with .', () => {
+  it('with A-Z ok', () => {
+    expect(emailPatternIsOk('DRYPOT@mail.test')).toBe(true)
+  })
+  it('with . ok', () => {
     expect(emailPatternIsOk('develop.bj@mail.test')).toBe(true)
   })
-  it('with -', () => {
+  it('with - ok', () => {
     expect(emailPatternIsOk('-a-b-c_d-e-f@mail.test')).toBe(true)
   })
-  it('incorrect mail', () => {
+  it('with * fail', () => {
+    expect(emailPatternIsOk('abc*xyz@mail.test')).toBe(false)
+  })
+  it('no @ fail', () => {
     expect(emailPatternIsOk('abc.mail.test')).toBe(false)
   })
-  it('with *', () => {
-    expect(emailPatternIsOk('abc*xyz@mail.test')).toBe(false)
+})
+
+describe('emailGetUserName', () => {
+  it('normal ok', () => {
+    expect(emailGetUserName('drypot@mail.test')).toBe('drypot')
+  })
+  it('with A-Z ok', () => {
+    expect(emailGetUserName('DRYPOT@mail.test')).toBe('DRYPOT')
+  })
+  it('with - ok', () => {
+    expect(emailGetUserName('-a-b-c_d-e-f@mail.test')).toBe('-a-b-c_d-e-f')
+  })
+  it('with * fail', () => {
+    expect(emailGetUserName('abc*xyz@mail.test')).toBeUndefined()
+  })
+  it('no @ fail', () => {
+    expect(emailGetUserName('abc.mail.test')).toBeUndefined()
   })
 })
--- src/ts/common/util/email.ts
+++ src/ts/common/util/email.ts
@@ -1,5 +1,10 @@
-const emailPattern = /^[a-z0-9-_+.]+@[a-z0-9-]+(\.[a-z0-9-]+)+$/i
+const emailPattern = /^([a-z0-9-_+.]+)@[a-z0-9-]+(\.[a-z0-9-]+)+$/i
 
 export function emailPatternIsOk(email: string) {
   return emailPattern.test(email)
+}
+
+export function emailGetUserName(email: string) {
+  const match = email.match(emailPattern)
+  return match?.[1]
 }
