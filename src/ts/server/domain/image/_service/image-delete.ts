e6e6114d962cd0ae62989889828c9192ddce0bff
drypot <drypot@gmail.com>
2021-10-31 16:25:23 +0900

    update image-delete-api

--- src/ts/server/domain/image/_service/image-delete.ts
+++ src/ts/server/domain/image/_service/image-delete.ts
@@ -1,5 +1,5 @@
 import { ErrorConst } from '@common/type/error'
-import { imageCheckUpdatable } from '@server/domain/image/_service/image-update'
+import { imageFindAndCheckUpdatable } from '@server/domain/image/_service/image-update'
 import { User } from '@common/type/user'
 import { ImageDB } from '@server/db/image/image-db'
 import { ImageFileManager } from '@server/fileman/_fileman'
@@ -7,10 +7,9 @@ import { ImageFileManager } from '@server/fileman/_fileman'
 export async function imageDelete(
   idb: ImageDB, ifm: ImageFileManager, user: User, id: number, err: ErrorConst[]
 ) {
-  const image = await imageCheckUpdatable(idb, user, id, err)
-  if (!image || err.length) {
-    return
-  }
+  const image = await imageFindAndCheckUpdatable(idb, user, id, err)
+  if (!image || err.length) return
+
   await idb.deleteImage(id)
   try {
     // 파일이 없을 경우 삭제하면 ENOENT 를 던진다.
