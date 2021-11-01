e119909d06744ce2d5aa8b5310c6b6dcd70e6d81
drypot <drypot@gmail.com>
2021-11-02 01:59:13 +0900

    add image-list fullscreen support

--- src/ts/client/util/dom.ts
+++ src/ts/client/util/dom.ts
@@ -1,3 +1,15 @@
 export function elementById(id: string) {
   return document.getElementById(id) as HTMLElement
 }
+
+export function newElementFromHtml(html: string) {
+  const template = document.createElement('template')
+  template.innerHTML = html.trim()
+  return template.content.firstElementChild as HTMLElement
+}
+
+export function newElementListFromHtml(html: string) {
+  const template = document.createElement('template')
+  template.innerHTML = html.trim()
+  return template.content.children
+}
---
+++ src/ts/client/util/fullscreen.ts
@@ -0,0 +1,48 @@
+
+export const fsWidth = window.screen.width * (window.devicePixelRatio || 1)
+export const fsWrapper = document.querySelector("#fullscreen") as HTMLElement
+
+const documentAny = document as any
+
+export const fsEnabled =
+  documentAny.fullscreenEnabled ||
+  documentAny.webkitFullscreenEnabled ||
+  documentAny.mozFullScreenEnabled ||
+  documentAny.msFullscreenEnabled
+
+export function fsRequest(el: HTMLElement) {
+  const elAny = el as any;
+  (elAny.requestFullscreen ||
+    elAny.webkitRequestFullscreen ||
+    elAny.mozRequestFullScreen ||
+    elAny.msRequestFullscreen
+  ).call(elAny)
+}
+
+export function fsExit() {
+  (documentAny.exitFullscreen ||
+    documentAny.mozCancelFullScreen ||
+    documentAny.webkitExitFullscreen ||
+    documentAny.msExitFullscreen
+  ).call(documentAny)
+}
+
+document.addEventListener('fullscreenchange', onChangeHandler)
+document.addEventListener('mozfullscreenchange', onChangeHandler)
+document.addEventListener('webkitfullscreenchange', onChangeHandler)
+document.addEventListener('MSFullscreenChange', onChangeHandler)
+
+function onChangeHandler() {
+  if (!fsInFullscreen()) {
+    fsWrapper.textContent = ''
+  }
+}
+
+function fsInFullscreen() {
+  return fsEnabled && !!(
+    documentAny.fullscreenElement ||
+    documentAny.mozFullScreenElement ||
+    documentAny.webkitFullscreenElement ||
+    documentAny.msFullscreenElement
+  )
+}
