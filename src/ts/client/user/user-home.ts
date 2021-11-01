a8344f3448d55ed258ac71899effce5330bff7e9
drypot <drypot@gmail.com>
2021-11-02 02:07:59 +0900

    add client user-home

---
+++ src/ts/client/user/user-home.ts
@@ -0,0 +1,11 @@
+
+export function initUserHome() {
+}
+
+//
+// cuser.initProfile = function () {
+//   let $profile = $('#profile-text')
+//   if ($profile.length) {
+//     $profile.html(tagUpText($profile.html()))
+//   }
+// }
