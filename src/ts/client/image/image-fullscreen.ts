9ff0ebcc08084277e72768282dddd940607f9aa4
drypot <drypot@gmail.com>
2021-11-10 06:10:29 +0900

    update css for rapixel, drypot, osoky

--- src/ts/client/image/image-fullscreen.ts
+++ src/ts/client/image/image-fullscreen.ts
@@ -1,7 +1,7 @@
 import { fsElement, fsExit, fsRequest, fsWidth } from '@client/util/fullscreen'
 import { newElementFromHtml } from '@client/util/dom'
 
-const fsIconTag = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="white">
+const fsIconTag = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="gray">
 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" />
 </svg>`
 
@@ -33,7 +33,7 @@ function findImageUrl(icon: HTMLElement) {
 }
 
 function openImage(url: string) {
-  const img = newElementFromHtml('<img src="' + url + '">')
+  const img = newElementFromHtml('<img src="' + url + '" class="img-full">')
   fsElement.textContent = ''
   fsElement.append(img)
   fsElement.onclick = img.onclick = function (e) {
