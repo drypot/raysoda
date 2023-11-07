c09de6fc68dcafbcdd387e4137205418c52cda54
drypot <drypot@gmail.com>
2023-11-07 22:16:44 +0900

    add sobeaut

--- src/ts/server/fileman/_fileman-loader.ts
+++ src/ts/server/fileman/_fileman-loader.ts
@@ -3,6 +3,7 @@ import { RapixelFileManager } from '@server/fileman/rapixel-fileman'
 import { OsokyFileManager } from '@server/fileman/osoky-fileman'
 import { DrypotFileManager } from '@server/fileman/drypot-fileman'
 import { RaySodaFileManager } from '@server/fileman/raysoda-fileman'
+import { SobeautFileManager } from '@server/fileman/sobeaut-fileman'
 
 import { getObject } from '@server/oman/oman'
 import { ImageFileManager } from '@server/fileman/_fileman'
@@ -13,6 +14,7 @@ export async function getImageFileManager(name: string): Promise<ImageFileManage
     rapixel: 'RapixelFileManager',
     osoky: 'OsokyFileManager',
     drypot: 'DrypotFileManager',
+    sobeaut: 'SobeautFileManager'
   }
   return await getObject(map[name])
 }
