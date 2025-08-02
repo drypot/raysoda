e9fd51e83973a8089b20afd907372be3f5941a0a
drypot <drypot@gmail.com>
2025-08-02 22:47:45 +0900

    update redirect

---
+++ src/domain/redirect/redirect-test.ts
@@ -0,0 +1,68 @@
+import supertest from 'supertest'
+import { getUserDB, UserDB } from '../../db/user/user-db.js'
+import { Express2, getExpress2 } from '../../express/express2.js'
+import { closeAllObjects, initObjectContext } from '../../oman/oman.js'
+import { useRedirect } from './redirect.js'
+import { insertUserFix4 } from '../../db/user/fixture/user-fix.js'
+
+describe('Redirect', () => {
+
+  let udb: UserDB
+  let express2: Express2
+  let agent: supertest.Agent
+
+  beforeAll(async () => {
+    initObjectContext('config/raysoda-test.json')
+    udb = await getUserDB()
+    express2 = await getExpress2()
+    await useRedirect()
+    await express2.start()
+    agent = supertest.agent(express2.server)
+  })
+
+  afterAll(async () => {
+    await closeAllObjects()
+  })
+
+  beforeAll(async () => {
+    await udb.dropTable()
+    await udb.createTable()
+    await insertUserFix4(udb)
+  })
+
+  it('/Com/Photo/View.aspx', async () => {
+    await agent.get('/Com/Photo/View.aspx?f=A&pg=3&p=937928')
+      .expect(301).expect('Location', '/image/937928')
+  })
+  it('/Com/Photo/List.aspx', async () => {
+    await agent.get('/Com/Photo/List.aspx?f=A&s=DD&pg=3')
+      .expect(301).expect('Location', '/')
+  })
+
+  it('/Com/Photo/CList.aspx', async () => {
+    await agent.get('/Com/Photo/CList.aspx?f=C')
+      .expect(301).expect('Location', '/')
+  })
+  it('/users', async () => {
+    await agent.get('/users/1')
+      .expect(301).expect('Location', '/user-id/1')
+  })
+  it('/images', async () => {
+    await agent.get('/images/1')
+      .expect(301).expect('Location', '/image/1')
+  })
+
+  it('/user1', async () => {
+    const res = await agent.get('/home1').expect(301).expect('Location', '/user/home1')
+  })
+  it('/USER1', async () => {
+    const res = await agent.get('/HOME1').expect(301).expect('Location', '/user/HOME1')
+  })
+  it('/xman', async () => {
+    const res = await agent.get('/xman').expect(404)
+  })
+  it('/xman/yman', async () => {
+    const res = await agent.get('/xman/yman').expect(404)
+  })
+
+})
---
+++ src/domain/redirect/redirect.ts
@@ -0,0 +1,43 @@
+import { getExpress2 } from '../../express/express2.js'
+import { getUserDB } from '../../db/user/user-db.js'
+
+export async function useRedirect() {
+
+  const express2 = await getExpress2()
+  const udb = await getUserDB()
+
+  express2.router.get('/Com/Photo/View.aspx', function (req, res) {
+    res.redirect(301, '/image/' + req.query.p)
+  })
+
+  express2.router.get('/Com/Photo/List.aspx', function (req, res) {
+    if (req.query.u) {
+      res.redirect(301, '/user/' + req.query.u)
+    } else {
+      res.redirect(301, '/')
+    }
+  })
+
+  express2.router.get('/Com/Photo/CList.aspx', function (req, res) {
+    res.redirect(301, '/')
+  })
+
+  express2.router.get('/users/:id', function (req, res) {
+    res.redirect(301, '/user-id/' + req.params.id)
+  })
+
+  express2.router.get('/images/:id', function (req, res) {
+    res.redirect(301, '/image/' + req.params.id)
+  })
+
+  express2.router.get('/:name', (req, res, done) => {
+    udb.getCachedByHome(req.params.name).then((user) => {
+      if (user) {
+        res.redirect(301, '/user/' + encodeURIComponent(req.params.name))
+        return
+      }
+      done()
+    }).catch(done)
+  })
+
+}
