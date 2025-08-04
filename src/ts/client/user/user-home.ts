837e61a71e41fa208ae0c2cbf95d0d072b4df070
drypot <drypot@gmail.com>
2025-08-04 23:10:16 +0900

    remove src/ts/client

--- src/ts/client/user/user-home.ts
+++
@@ -1,7 +0,0 @@
-import { getElementById } from '@client/util/dom'
-import { tagUp } from '@common/util/tagup'
-
-export function initUserHome() {
-  const profile = getElementById("profile")
-  profile.innerHTML = tagUp(profile.innerHTML)
-}
--- src/ts/client/util/refresh.ts
+++
@@ -1,8 +0,0 @@
-export function setRefresh() {
-  sessionStorage.setItem('refresh', 'true')
-}
-
-if (sessionStorage.getItem('refresh') === 'true') {
-  sessionStorage.removeItem('refresh')
-  window.location.reload()
-}
