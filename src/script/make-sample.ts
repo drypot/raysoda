ebb40a03bfa97dac757f754e83c9c3fd4f4da507
drypot <drypot@gmail.com>
2025-08-11 02:15:35 +0900

    update make-sample

--- src/script/make-sample.ts
+++ src/script/make-sample.ts
@@ -1,5 +1,6 @@
 import { exec } from 'child_process'
 import { logError } from '../common/util/error2.ts'
+import sharp from 'sharp'
 
 /*
   $ node sample/make-sample.js
@@ -9,6 +10,61 @@ process.on('uncaughtException', function (err) {
   console.error(err)
   process.exit(1)
 })
+
+async function writeImage(w: number, h: number) {
+  const lx = w - 1
+  const ly = h - 1
+  const cx = lx / 2;
+  const cy = ly / 2;
+
+  // let cmd = ''
+  // cmd += 'convert -size ' + w + 'x' + h + ' xc:#c0c0c0'
+  // cmd += ' -fill "#c0c0c0" -stroke "#303030" '
+  // if (w > h) {
+  //   cmd += ' -draw "circle ' + cx + ', ' + cy + ', ' + cx + ', 0"'
+  //   cmd += ' -draw "circle ' + cx + ', ' + cy + ', ' + cx + ', ' + ly / 4 + '"'
+  // } else {
+  //   cmd += ' -draw "circle ' + cx + ', ' + cy + ', ' + '0, ' + cy + '"'
+  //   cmd += ' -draw "circle ' + cx + ', ' + cy + ', ' + lx / 4 + ', ' + cy + '"'
+  // }
+  // cmd += ' -draw "line 0,0 ' + lx + ',' + ly + ' line 0,' + ly + ' ' + lx + ',0"'
+  // cmd += ' -quality 92 sample/' + w + 'x' + h + '.jpg'
+
+  // console.log(cmd)
+
+  // await new Promise<void>((resolve, reject) => {
+  //   exec(cmd, (err) => {
+  //     if (err) return reject(err)
+  //     resolve()
+  //   })
+  // })
+
+  const r1 = Math.min(lx, ly) / 2
+  const r2 = Math.min(lx, ly) / 4
+
+  const hx = w > h ? cx * 0.25 : cx
+  const hy = w > h ? cy : cy * 0.25
+
+  const svg = `
+    <svg width="${w}" height="${h}">
+      <rect width="100%" height="100%" fill="#c0c0c0"/>
+      <g fill="none" stroke="#303030">
+        <circle cx="${cx}" cy="${cy}" r="${r1}" />
+        <circle cx="${cx}" cy="${cy}" r="${r2}" />
+        <line x1="0" y1="0" x2="${lx}" y2="${ly}" />
+        <line x1="0" y1="${ly}" x2="${lx}" y2="0" />
+        <line x1="${cx}" y1="${cy}" x2="${hx}" y2="${hy}" />
+      </g>
+    </svg>`.trim()
+
+  const outfile = `sample/${w}x${h}.jpg`
+
+  await sharp(Buffer.from(svg))
+    .jpeg({ quality: 92 })
+    .toFile(outfile)
+
+  console.log(outfile)
+}
 
 async function main() {
   const _vers = [
@@ -27,29 +83,7 @@ async function main() {
     { width: 1440, height: 2560 } // raysoda
   ]
   for (const v of _vers) {
-    const w = v.width
-    const h = v.height
-    const lx = w - 1
-    const ly = h - 1
-    let cmd = ''
-    cmd += 'convert -size ' + w + 'x' + h + ' xc:#c0c0c0'
-    cmd += ' -fill "#c0c0c0" -stroke "#303030" '
-    if (w > h) {
-      cmd += ' -draw "circle ' + lx / 2 + ', ' + ly / 2 + ', ' + lx / 2 + ', 0"'
-      cmd += ' -draw "circle ' + lx / 2 + ', ' + ly / 2 + ', ' + lx / 2 + ', ' + ly / 4 + '"'
-    } else {
-      cmd += ' -draw "circle ' + lx / 2 + ', ' + ly / 2 + ', ' + '0, ' + ly / 2 + '"'
-      cmd += ' -draw "circle ' + lx / 2 + ', ' + ly / 2 + ', ' + lx / 4 + ', ' + ly / 2 + '"'
-    }
-    cmd += ' -draw "line 0,0 ' + lx + ',' + ly + ' line 0,' + ly + ' ' + lx + ',0"'
-    cmd += ' -quality 92 sample/' + w + 'x' + h + '.jpg'
-    console.log(cmd)
-    await new Promise<void>((resolve, reject) => {
-      exec(cmd, (err) => {
-        if (err) return reject(err)
-        resolve()
-      })
-    })
+    await writeImage(v.width, v.height)
   }
 }
 
